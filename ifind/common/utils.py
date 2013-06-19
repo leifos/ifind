b117a3bde5cea55079f190b813576d7c7c59b3ce
leif <leifos@acm.org>
2013-06-19 20:03:46 +0100

    Sorted out the models so it now syncdb's

---
+++ ifind/common/utils.py
@@ -0,0 +1,35 @@
+__author__ = 'leif'
+
+
+def take_screen_shot(url, path, name, height, width):
+    """ given the url, take a screen shot and save it to the path/name with dimensions (height, width)
+
+    Args:
+        url: string to url of web page
+        path: string of where to save the image
+        height: integer (size of screen shot)
+        width: integer
+
+    Returns:
+        None
+    """
+
+    pass
+
+
+
+def perform_query(query_terms):
+    """ issue the query terms to bing
+    Args:
+        query_terms: string contain the query
+
+    Returns:
+        result: list of titles and urls in rank order
+
+    """
+
+    pass
+
+
+
+
--- ifind/models/__init__.py
+++ ifind/models/__init__.py
@@ -1,1 +1,3 @@
 __author__ = 'leif'
+__all__ = ['models']
+from test import *
