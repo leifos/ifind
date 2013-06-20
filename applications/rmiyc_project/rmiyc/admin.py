f3676b98c8c35aa66f8075c91e09687f826ec93e
arazzouk <abdullah.rafik.razzouk@gmail.com>
2013-06-20 15:16:52 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	framework/models/game_models.py

---
+++ applications/rmiyc_project/rmiyc/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from models import *
+
+admin.site.register(Achievement)
+admin.site.register(Category)
+admin.site.register(CurrentGame)
+admin.site.register(Page)
+admin.site.register(UserProfile)
+admin.site.register(Score)
+admin.site.register(PlayerAchievement)
+admin.site.register(Levels)
+
+
+
+
