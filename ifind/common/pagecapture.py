1c93ed47c1154b3990450f278e911495e95f03bf
mtbvc <1006404b@student.gla.ac.uk>
2013-06-24 10:29:35 +0100

    Some changes to pagecapture and capture_screenshot

--- ifind/common/pagecapture.py
+++ ifind/common/pagecapture.py
@@ -4,21 +4,42 @@ from ifind.common import utils
 
 class PageCapture:
 
-    def __init__(self,width,height):
+    def __init__(self,url=None,width=800,height=600):
         self.driver = utils.webdriver.PhantomJS()
         self.driver.set_window_size(width,height)
+        if url is not None:
+            self.load_url(url)
+        else:
+            self.url = url
 
-    def get_webpage(self,url):
-        """ url format: 'http://www.example.com' """
+    def load_url(self,url):
+        """ url format: 'http://www.example.com'
+
+        args: take a url string
+        returns: true if the page is loaded in the driver, else false
+
+        """
+
+        #check to make sure the url is loaded.
         self.driver.get(url)
+        # if driver is loaded
+        self.url = url
+        return True
+        # else:
+        # self.url = None
+        # return False
 
-    def take_screen_shot(self, url, path, name, height, width):
-        utils.take_screen_shot(url, path, name, height, width)
+    def take_screen_shot(self, url, filename , height, width):
+        if self.url:
+            #self.driver
+            utils.take_screen_shot(url, '', name, height, width)
 
-    def crop_screen_shot(self, source, destination, x1, y1, x2, y2):
-        utils.crop_screen_shot(source, destination, x1, y1, x2, y2)
+    def crop_screen_shot(self, filename, x1, y1, x2, y2):
+        utils.crop_screen_shot(destination, x1, y1, x2, y2)
 
-    def halve_screen_shot(self, source, destination):
+    def halve_screen_shot(self, filename):
+        """ args: filename - where the screen shot will be saved to.
+        """
         utils.halve_screen_shot(source, destination)
 
     def get_page_title(self):
@@ -26,5 +47,4 @@ class PageCapture:
 
     def get_page_sourcecode(self):
         return self.driver.page_source
-
 
