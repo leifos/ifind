41b3bd5373ec4670c395466387f4b075ab59151c
arazzouk <abdullah.rafik.razzouk@gmail.com>
2013-06-25 12:32:09 +0100

    polished the code added check url function
    Conflicts:
    	framework/models/game_models.py

--- applications/rmiyc_project/populate_category.py
+++ applications/rmiyc_project/populate_category.py
@@ -7,17 +7,15 @@ setup_environ(settings)
 from rmiyc.models import Page, Category
 import os
 from django.core.files import File
+import httplib
+from urlparse import urlparse
 
 
 def convert_url_to_filename(url):
-    """Take a url string and return a valid filename constructed from the string.
-    Uses a whitelist approach: any characters not present in valid_chars are
-    removed. Also spaces are replaced with underscores.
 
-    Note: this method may produce invalid filenames such as ``, `.` or `..`
-    When I use this method I prepend a date string like '2009_01_15_19_46_32_'
-    and append a file extension like '.txt', so I avoid the potential of using
-    an invalid filename.
+    '''
+    :param url: Takes a url string
+    :return: a valid filename constructed from the string
 
     >>> convert_url_to_filename("http://www.google.com")
     'www-google-com'
@@ -25,30 +23,59 @@ def convert_url_to_filename(url):
     'www-dcs-gla-ac-uk-leif-index-html'
     >>> convert_url_to_filename("www.ball.com/dj")
     'www-ball-com-dj'
-    """
-    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    '''
+
+    # Remove the http:// from the url as it is common among all the urls
+    url = url.replace('http://', '')
+    valid_chars = "-_.()/ %s%s" % (string.ascii_letters, string.digits)
+    # Uses a whitelist approach: any characters not present in valid_chars are removed
+    # spaces,slashes and dots are replaced with dashes
     filename = ''.join(c for c in url if c in valid_chars)
-    filename = filename.replace(' ','_') # to remove spaces in file names.
+    filename = filename.replace(' ', '-')  # to replace spaces in file names with dashes.
+    filename = filename.replace('/', '-')  # to replace slashes in file names with dashes.
+    filename = filename.replace('.', '-')  # to remove dots in file names.
     return filename
 
 
 def read_in_urls(filename):
     # read in file - store in a list (url_list)
-    ## Open the file with read only permit
+    # Open the file with read only permit
     f = open(filename, 'r')
-    ## Read the first line
+    # Read the first line
     print 'file is opened'
     url_list = []
     for line in f:
-        # clean up the url
+        # Strip urls from spaces
         url = line.strip()
-        # validate the url
         # is it a well-formed url string
-        url_list.append(url)
+        # Abdullah: url format rules are so flexible so it doesn't make sense (for me) to validate them
+        # validate the url
+        if checkUrl(url):
+            url_list.append(url)
 
     f.close()
     print 'file is closed'
     return url_list
+
+
+def checkUrl(url):
+    '''
+    :param url: takes a url string
+    :return: true if the url exists on the web
+            false: if the url does not exist on the web
+    >>> urlparse('//www.cwi.nl:80/%7Eguido/Python.html')
+    ParseResult(scheme='', netloc='www.cwi.nl:80', path='/%7Eguido/Python.html', params='', query='', fragment='')
+    >>> urlparse('www.cwi.nl/%7Eguido/Python.html')
+    ParseResult(scheme='', netloc='', path='www.cwi.nl:80/%7Eguido/Python.html', params='', query='', fragment='')
+    >>> urlparse('help/Python.html')
+    ParseResult(scheme='', netloc='', path='help/Python.html', params='', query='', fragment='')
+
+    '''
+    p = urlparse(url)
+    conn = httplib.HTTPConnection(p.netloc)
+    conn.request('HEAD', p.path)
+    resp = conn.getresponse()
+    return resp.status < 400
 
 
 def get_category(category_name, desc='', icon='', append=False):
@@ -76,32 +103,27 @@ def get_category(category_name, desc='', icon='', append=False):
 def populate_pages(url_list, category):
     """
 
-    :param url_list:
-    :param category:
+    :param url_list: a list of the urls for the pages that are going to be populated
+    :param category: the category in which the pages fall into
     :return:
     """
-    # create PageCapture object - specify the browser to be 800 x 600.
 
     #For each url in the url_list
     for url in url_list:
-        # convert the url to a filename os.path.join()
-        # This added a back slash / at the end of the url which caused problems
-        # Strip urls from spaces
 
+        # create PageCapture object - specify the browser to be 800 x 600.
         pc = PageCapture(url,800, 600)
         url_file_name = convert_url_to_filename(url)+'.png'
-        image_file_name = os.path.join(os.getcwd(),'imgs',url_file_name)
-        print image_file_name
+        image_file_name = os.path.join(os.getcwd(), 'imgs', url_file_name)
         pc.load_url(url)
         # fetch the screen-shot
-        pc.fetch_screen_shot(image_file_name)
+        pc.take_screen_shot(image_file_name)
         # get the title
         title = pc.get_page_title()
         # create page in models/db with category
-        Page(category=category, title=title, is_shown=True, url=url, screenshot =image_file_name).save()
-        print 'Page title= ' + title + ' has been saved!'
-
-
+        p = Page(category=category, title=title, is_shown=True, url=url, screenshot =image_file_name)
+        p.save()
+        print 'Page title= ' + p.title + ' has been saved!'
 
 
 def main(file_name, category_name, append):
