55d6c64cabd1dfa535155dfaa720553ea8eb898c
arazzouk <abdullah.rafik.razzouk@gmail.com>
2013-06-25 22:24:33 +0100

    polished the code added check url function
    Conflicts:
    	framework/models/game_models.py

---
+++ applications/rmiyc_project/rmiyc/bing_search.py
@@ -0,0 +1,47 @@
+import urllib, urllib2
+import json
+
+
+def run_query(search_terms):
+    '''Issues a query to the Bing Search API.
+    Args:
+        search_terms: is a string containing the query terms
+    Returns:
+        results: a list where each record is a dict containing: title, link and summary.
+    Raises:
+        urllib exception
+    '''
+
+    # parameters for the search request
+    root_url = 'https://api.datamarket.azure.com/Bing/Search/'
+    source = 'Web' # source could be: Image, News, RelatedSearch, Video, or Web
+    results_per_page = 10
+    offset = 0
+    # Bing API expects the query to be in quotes, and quoted! Strange but True.
+    query = "'"+ search_terms +"'"
+    quoted_query = urllib.quote(query)
+    # Construct the URL / search request
+    search_url = "%s%s?$format=json&$top=%d&$skip=%d&Query=%s" % (root_url, source, results_per_page, offset, quoted_query)
+
+    # Add the API key to the password manager. There IS no username.
+    username = ''
+    bing_api_key = 'm8KBb9SgQxAAnoAl6PJRnCqdaaKQKVk3Z+iWnj4OR5s'
+    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, search_url, username, bing_api_key)
+
+    results = []
+    try:
+        # Prepare an authentication handler and open the URL
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib2.build_opener(handler)
+        urllib2.install_opener(opener)
+        response = urllib2.urlopen(search_url).read()
+        # Convert the response to json and parse out the fields (title, link, and summary)
+        json_response = json.loads(response)
+        for result in json_response['d']['results']:
+            results.append({'title': result['Title'], 'link': result['Url'], 'summary': result['Description']} )
+
+    except urllib2.URLError, e:
+        print "Error when querying Bing API", e
+
+    return results
--- applications/rmiyc_project/rmiyc/views.py
+++ applications/rmiyc_project/rmiyc/views.py
@@ -3,25 +3,23 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from ifind.search.engine.bing_web_searchv2 import BingWebSearch
 from ifind.search.query import Query
+from bing_search import run_query
+
 
 # Create your views here.
 def index(request):
         # select the appropriate template to use
         template = loader.get_template('rmiyc/base.html')
-        # create and define the context. We don't have any context at the moment
-        # but later on we will be putting data in the context which the template engine
-        # will use when it renders the template into a page.
         context = RequestContext(request, {})
         # render the template using the provided context and return as http response.
         return HttpResponse(template.render(context))
 
 
-def play(request):
+def play(request, category_name):
         # select the appropriate template to use
         template = loader.get_template('rmiyc/game.html')
-        # create and define the context. We don't have any context at the moment
-        # but later on we will be putting data in the context which the template engine
-        # will use when it renders the template into a page.
+        # Query the database for the provided category name
+
         context = RequestContext(request, {})
         # render the template using the provided context and return as http response.
         return HttpResponse(template.render(context))
@@ -30,20 +28,32 @@ def play(request):
 def pick_category(request):
         # select the appropriate template to use
         template = loader.get_template('rmiyc/cat_picker.html')
-        # create and define the context. We don't have any context at the moment
-        # but later on we will be putting data in the context which the template engine
-        # will use when it renders the template into a page.
         context = RequestContext(request, {})
         # render the template using the provided context and return as http response.
         return HttpResponse(template.render(context))
+
 
 def search(request):
         context = RequestContext(request)
         if request.method == 'POST':
             query = request.POST['query'].strip()
             if query:
+                print query
                 query = Query(query, source_type="Web", format='JSON')
                 search_engine = BingWebSearch(api_key="m8KBb9SgQxAAnoAl6PJRnCqdaaKQKVk3Z+iWnj4OR5s")
                 result = search_engine.search(query)
+                print result
 
-        return render_to_response('rmiyc/Game.html',{ 'result_list': result.result_list }, context)
+
+        return render_to_response('rmiyc/Game.html',{ 'result_list': result.results }, context)
+
+
+def search2(request):
+        context = RequestContext(request)
+        result_list = []
+        if request.method == 'POST':
+            query = request.POST['query'].strip()
+            if query:
+                result_list = run_query(query)
+
+        return render_to_response('rmiyc/Game.html',{ 'result_list': result_list }, context)
