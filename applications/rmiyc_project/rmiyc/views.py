758adba2d53f03579d750c60cc71b81732c68155
arazzouk <abdullah.rafik.razzouk@gmail.com>
2013-06-28 17:38:53 +0100

    polished the code added check url function
    Conflicts:
    	framework/models/game_models.py

--- applications/rmiyc_project/rmiyc/views.py
+++ applications/rmiyc_project/rmiyc/views.py
@@ -4,36 +4,62 @@ from django.shortcuts import render_to_response
 from ifind.search.engine.bing_web_searchv2 import BingWebSearch
 from ifind.search.query import Query
 from bing_search import run_query
-
-
+from keys import BING_API_KEY
+from ifind.models.game_mechanics import GameMechanic
+from ifind.models.game_models import Category
+from django.contrib.auth.models import User
+from datetime import datetime
 # Create your views here.
+
 def index(request):
-        # select the appropriate template to use
 
-        template = loader.get_template('rmiyc/base.html')
         context = RequestContext(request, {})
-
-        # render the template using the provided context and return as http response.
-        return HttpResponse(template.render(context))
+        return render_to_response('rmiyc/base.html', context)
 
 
 def play(request, category_name):
         print 'I have been played'
-        # select the appropriate template to use
-        template = loader.get_template('rmiyc/game.html')
+        '''
+            if request.session.test_cookie_worked():
+                print "The test cookie worked!!!"
+                request.session.delete_test_cookie()
+        '''
         # Query the database for the provided category name
-        context = RequestContext(request, {})
-
         # render the template using the provided context and return as http response.
-        return HttpResponse(template.render(context))
+        user = User.objects.get(username__exact ='john')
+        if not user:
+            user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+            user.save()
+        c = Category.objects.get(name=category_name)
+        gm = GameMechanic()
+        if request.COOKIES.has_key('game_id'):
 
+            print 'My game id is ' + request.COOKIES.get('game_id')
+            game_id = request.COOKIES.get('game_id')
+            gm.retrieve_game(user,game_id)
+            gm.take_points()
+            gm.set_next_page()
+            request.session.set_expiry(0)
+        else:
+            print 'Im waiting to be assigned to a value'
+            gm.create_game(user,c ,0)
+
+
+
+        game_id= gm.get_game_id()
+        print 'my game_id is:%d ' % (game_id)
+        p = gm.get_current_page()
+
+        context = RequestContext(request, {})
+        response = render_to_response('rmiyc/game.html', context)
+        response.set_cookie('game_id', game_id)
+        return response
 
 def pick_category(request):
-        # select the appropriate template to use
-        template = loader.get_template('rmiyc/cat_picker.html')
         context = RequestContext(request, {})
         # render the template using the provided context and return as http response.
-        return HttpResponse(template.render(context))
+        request.session.set_test_cookie()
+        return render_to_response('rmiyc/cat_picker.html', context)
 
 
 def search(request):
@@ -43,7 +69,7 @@ def search(request):
             if query:
                 print query
                 query = Query(query, source_type="Web", format='JSON')
-                search_engine = BingWebSearch(api_key="m8KBb9SgQxAAnoAl6PJRnCqdaaKQKVk3Z+iWnj4OR5s")
+                search_engine = BingWebSearch(api_key=BING_API_KEY)
                 result = search_engine.search(query)
                 print result
 
@@ -57,11 +83,13 @@ def search2(request):
             query = request.POST['query'].strip()
             if query:
                 result_list = run_query(query)
-        template = loader.get_template('rmiyc/search_results.html')
+        template = loader.get_template('rmiyc/game.html')
         context = RequestContext(request, {'result_list': result_list })
         return HttpResponse(template.render(context))
 
 
-def test(request):
-        print 'Test has beeen called '
-        pass
+def game_over(request):
+        request.session.set_expiry(datetime.now())
+        print 'I am a cookie and I am dying because the game is over'
+        context = RequestContext(request, {})
+        return render_to_response('rmiyc/cat_picker.html', context)
--- ifind/models/game_mechanics.py
+++ ifind/models/game_mechanics.py
@@ -63,7 +63,7 @@ class GameMechanic(object):
         # if found, set self.game to game record
         if cg:
             self.game = cg
-            self.set_pages_to_use(game_type)
+            self.set_pages_to_use(0)
             found = True
 
         return found
@@ -89,6 +89,12 @@ class GameMechanic(object):
 
     def get_current_score(self):
         return self.game.current_score
+
+    def get_game_id(self):
+        return self.game.id
+
+    def get_current_page(self):
+        return self.game.current_page
 
     def _increment_queries_issued(self, query_successful=False):
         self.game.no_of_queries_issued += 1
