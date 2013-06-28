1e34ffd4cfbad9c0710bb253982e5ca481ae64e3
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-06-28 19:35:14 +0100

    created test folder for formal/informal tests

---
+++ ifind/search/tests/bing_test.py
@@ -0,0 +1,22 @@
+# TODO Clean up, rename and convert into BingSearch CLI module
+
+from ifind.search.engine import EngineFactory
+from ifind.search.query import Query
+
+
+query = Query('one', source_type="image", format='atom')
+#query = Query('Hello World', source_type="Web", format='ATOM')
+#query = Query('Hello World', source_type="Web", format='JSON', top=60, skip=1)
+
+
+#result = search_engine.search(query)
+
+#for r in result.results:
+
+engine = EngineFactory("bing", api_key='5VP0SQJkCyzkT1GfsWT//q4pt1zxvyaVVhltoDhfTDQ')
+
+result = engine.search(query)
+
+for r in result.results:
+    print r['title']
+
