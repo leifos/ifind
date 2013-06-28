534faeef8dd921c6be8596cbc7a4b6e330faae65
leif <leifos@acm.org>
2013-06-28 14:48:53 +0100

    engine

--- ifind/search/engine.py
+++ ifind/search/engine.py
@@ -1,3 +1,16 @@
+def EngineFactory(engine_string, **kwargs):
+    return get_class(engine_string, **kwargs)
+
+
+def get_class(engine_string, **kwargs):
+    import imp
+    import ifind.search.engines as engines
+    module_path = engines.__path__[0] + '/' + engine_string.lower() + '.py'
+    module = imp.load_source('pass', module_path)
+
+    return getattr(module, engine_string.lower().title())(**kwargs)
+
+
 class Engine(object):
     """
     Abstract search engine interface.
