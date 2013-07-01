03c43939de08edd6d50790798eb4d135602996ec
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-01 11:07:08 +0100

    moved bing test

--- ifind/search/tests/bing_test.py
+++ ifind/search/tests/bing_test.py
@@ -4,7 +4,7 @@ from ifind.search.engine import EngineFactory
 from ifind.search.query import Query
 
 
-query = Query('one', source_type="image", format='atom')
+query = Query('one', source_type="WEB", format='atom')
 #query = Query('Hello World', source_type="Web", format='ATOM')
 #query = Query('Hello World', source_type="Web", format='JSON', top=60, skip=1)
 
