e6fb5b4e85a6ffb671e74b5fa2d9dbefd3821501
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-01 13:34:07 +0100

    Exceptions sorted for most part

--- ifind/search/tests/bing_test.py
+++ ifind/search/tests/bing_test.py
@@ -2,21 +2,32 @@
 
 from ifind.search.engine import EngineFactory
 from ifind.search.query import Query
+import redis
 
 
-query = Query('one', source_type="WEB", format='atom')
+#query = Query('test', source_type="WEB", format='ATOM')
 #query = Query('Hello World', source_type="Web", format='ATOM')
-#query = Query('Hello World', source_type="Web", format='JSON', top=60, skip=1)
-
-
-#result = search_engine.search(query)
-
-#for r in result.results:
+query = Query('chicken', source_type="Web", format='JSON', top=30, skip=0)
+query2 = Query('chicken', source_type="Web", format='JSON', top=10, skip=400)
 
 engine = EngineFactory("bing", api_key='5VP0SQJkCyzkT1GfsWT//q4pt1zxvyaVVhltoDhfTDQ')
 
 result = engine.search(query)
+result2 = engine.search(query2)
 
-for r in result.results:
-    print r['title']
+#r_server = redis.Redis("localhost")
 
+for i, r in enumerate(result.results):
+    print i, r['title']
+    #r_server.sadd('result25', r['url'])
+
+print
+
+for i, r in enumerate(result2.results):
+    print i, r['title']
+    #r_server.sadd('result26', r['url'])
+
+#print r_server.scard('result25')
+#print r_server.scard('result26')
+
+#r_server.sinter('dog', 'dogg')
