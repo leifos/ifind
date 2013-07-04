7c007f8996b49f5a722cd724ef0de56c5cec8310
arazzouk <abdullah.rafik.razzouk@gmail.com>
2013-07-04 21:05:17 +0100

    Integrated with authentication app,
    Conflicts:
    	framework/models/game_models.py

---
+++ applications/rmiyc_project/static/javascript/search_ajax.js
@@ -0,0 +1,75 @@
+$(function()
+{
+    window.onbeforeunload = confirmExit;
+    function confirmExit()
+    {
+        return false;
+    }
+
+    $(document).keypress(function(event)
+    {
+        if(event.which == 13 && event.ctrlKey)
+        {
+            event.preventDefault();
+            $.ajax
+            ({
+                type: "GET",
+                url: "/rmiyc/display_next_page/",
+                success: display_next_page_success,
+                dataType: 'html'
+            });
+        }
+    });
+
+    $('#search_form').submit(function(event)
+    {
+        event.preventDefault();
+        $.ajax
+        ({
+            type: "POST",
+            url: "/rmiyc/search2/",
+            data:
+            {
+                'query' : $('#query').val(),
+                'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
+            },
+            success: search_success,
+            dataType: 'html'
+        });
+
+    });
+
+    $('#skip').click(function(event)
+    {
+        event.preventDefault();
+        $.ajax
+        ({
+            type: "GET",
+            url: "/rmiyc/display_next_page/",
+            success: display_next_page_success,
+            dataType: 'html'
+        });
+    });
+});
+
+function search_success(data, textStatus, jqXHR)
+{
+    $('#content').html(data);
+    $('#query').val("");
+    var x = $('#score-variable').text();
+    if(x!= 0)
+    {
+        $('#skip').text('take points');
+        $('#search').text('search again');
+    }
+}
+
+function display_next_page_success(data, textStatus, jqXHR)
+{
+    $('#skip').text('skip');
+    $('#search').text('search');
+    $('#image-box').hide();
+    $('#score-div').hide();
+    $('#search-results-div').hide();
+    $('#image-div').html(data);
+}
