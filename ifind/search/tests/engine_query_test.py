0a32dc37569e9d9efb2bb4db9dfa6b61b28261d8
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-04 11:02:07 +0100

    wikipedia search prototype works

--- ifind/search/tests/engine_query_test.py
+++ ifind/search/tests/engine_query_test.py
@@ -3,21 +3,20 @@ import xml.dom.minidom
 from ifind.search.engine import EngineFactory
 from ifind.search.query import Query
 
-query = Query('test', result_type="Web", format='JSON', top=70)
+query = Query('test', result_type="WEB", format='JSON')
 #query = Query('Hello World', source_type="Web", format='ATOM')
 #query = Query('awful bus glasgow', source_type="Web", format='JSON', top=10, skip=0)
 #query2 = Query('milk death', result_type="web", format='JSON', top=70, skip=0)
 #query2 = Query("furnace", result_type='recent', top=5)
 
-engine = EngineFactory("bing", api_key='5VP0SQJkCyzkT1GfsWT//q4pt1zxvyaVVhltoDhfTDQ')
+engine = EngineFactory("bing", api_key='')
 
 
-response = engine.search(query)
+#response = engine.search(query)
 
-
-for index, result in enumerate(response):
-    print index
-    print result
+#for index, result in enumerate(response):
+    #print index
+    #print result
 
 
 # result2 = engine.search(query2)
@@ -40,7 +39,27 @@ for index, result in enumerate(response):
 #
 # #r_server.sinter('dog', 'dogg')
 
-#/
-    #slug = re.findall(r'')
+terms = "dog"
 
-#items = xml.getElementByTagName('Item')
+search_params = {'format': 'xml',
+                 'search': terms,
+                 'action': 'opensearch'}
+
+response = requests.get('http://www.wikipedia.org/w/api.php', params=search_params)
+
+
+xmldoc = xml.dom.minidom.parseString(response.content)
+
+print xmldoc.toprettyxml()
+
+items = xmldoc.getElementsByTagName('Item')
+
+for item in items:
+
+    title = item.getElementsByTagName('Text')[0].firstChild.data
+    url = item.getElementsByTagName('Url')[0].firstChild.data
+    summary = item.getElementsByTagName('Description')[0].firstChild.data
+
+    print title
+    print summary
+    print url
