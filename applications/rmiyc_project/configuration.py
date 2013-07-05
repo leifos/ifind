eb7ed0c1372b4e3ac9e4c0eb234485c922f42ea6
arazzouk <abdullah.rafik.razzouk@gmail.com>
2013-07-05 12:13:32 +0100

    Integrated with authentication app,
    Conflicts:
    	framework/models/game_models.py

---
+++ applications/rmiyc_project/configuration.py
@@ -0,0 +1,18 @@
+__author__ = 'arazzouk'
+import os
+
+PROJ_PATH = os.getcwd()
+GAME_DB = os.path.join(PROJ_PATH, 'rmiyc.db')
+TEMP_PATH = os.path.join(PROJ_PATH, 'templates')
+STATIC_PATH = os.path.join(PROJ_PATH, 'static/')
+
+MEDIA_PATH = ''
+MEDIA_URL = ''
+MEDIA_ROOT = 'imgs/'
+UPLOAD_DIR = os.path.join(os.getcwd(), 'imgs/')
+
+
+DEPLOY = False
+DEBUG = True
+
+APP_NAME = 'rmiyc'
