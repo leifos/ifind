3ecfb31f2cbff3ff2b8a4887bae574ecf3b9ec8b
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-05 16:51:10 +0100

    minor tidy

--- ifind/search/engines/exceptions.py
+++ ifind/search/engines/exceptions.py
@@ -13,14 +13,12 @@ class SearchException(Exception):
     def __init__(self, engine, message):
 
         self.engine = engine + 'Exception'
-
         Exception.__init__(self, message)
 
         class NewClass(ValueError):
             pass
 
         NewClass.__name__ = self.engine
-
         raise NewClass(self.message)
 
 
@@ -29,8 +27,6 @@ class EngineException(SearchException):
     def __init__(self, engine, message, code=None):
 
         self.code = code
-
         if code:
             self.message = ERROR.get(code, ERROR['default']).format(self.code)
-
         SearchException.__init__(self, engine, message)
