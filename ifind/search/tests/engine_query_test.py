e3442eb1826fba1ec297f1f96800f33a2829a38b
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-05 16:50:01 +0100

    nothing new, just saving

--- ifind/search/tests/engine_query_test.py
+++ ifind/search/tests/engine_query_test.py
@@ -1,25 +1,18 @@
-from ifind.search.engines.exceptions import SearchException
+
 from ifind.search.engine import EngineFactory
 from ifind.search.query import Query
 #from ifind.search.cache import *
 
 #query = Query('dog', top=10)
-#query = Query('Hello World', source_type="Web", format='ATOM')
+query = Query('Hello World', top=10)
 #query = Query('awful bus glasgow', source_type="Web", format='JSON', top=10, skip=0)
 #query2 = Query('milk death', result_type="web", format='JSON', top=70, skip=0)
 #query2 = Query("furnace", result_type='recent', top=5)
 
 #raise SearchException(__file__.split('/')[-1].split('.')[0], "PROBLEM")
 
-#engine = EngineFactory("twitter")
+engine = EngineFactory("wikipedia")
 
-#response = engine.search(query)
+response = engine.search(query)
 
-query = Query('Hello World', source_type="Web", format='ATOM')
-query2 = Query('awful bus glasgow', source_type="Web", format='JSON')
-
-query2.terms = 'Hello World'
-query2.format = "ATOM"
-
-
-print query == query2
+print response
