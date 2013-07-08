fd09238894579c1fc7c7fbfdd9e43284557421e8
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-08 11:44:14 +0100

    nothing new just keeping it relevant

--- ifind/search/tests/engine_query_test.py
+++ ifind/search/tests/engine_query_test.py
@@ -4,7 +4,7 @@ from ifind.search.query import Query
 #from ifind.search.cache import *
 
 #query = Query('dog', top=10)
-query = Query('Hello World', top=10)
+query = Query('glasgow', top=10)
 #query = Query('awful bus glasgow', source_type="Web", format='JSON', top=10, skip=0)
 #query2 = Query('milk death', result_type="web", format='JSON', top=70, skip=0)
 #query2 = Query("furnace", result_type='recent', top=5)
