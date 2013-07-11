33afcfd885f13128274d7eef86a839e6056e5a70
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-11 13:31:13 +0100

    tests for expire and store/retrieve

--- ifind/search/tests/test_cache.py
+++ ifind/search/tests/test_cache.py
@@ -1,18 +1,31 @@
+import mock
 from nose.tools import assert_equal
 from nose.tools import assert_not_equal
 from nose.tools import assert_raises
 
 import ifind.search.cache as cache
-from ifind.search.engine import EngineFactory
+from ifind.search.query import Query
+from ifind.search.response import Response
+
 
 class TestCache(object):
 
     def setup(self):
-        engine = EngineFactory("twitter")
-        #self.c = cache.QueryCache(engine)
-        assert_raises(DynamicException, cache.QueryCache, engine)
 
+        engine = mock.Mock()
+        engine.name = "TestEngine"
+        engine.cache_type = 'engine'
 
-    def test_cache(self):
-        assert_raises(DynamicException, EngineFactory, "bing")
-        pass
+        self.c = cache.QueryCache(engine)
+        self.query = Query('hello world')
+        self.response = Response('hello world')
+
+    def test_expires(self):
+        import time
+        self.c.store(self.query, self.response, expires=1)
+        time.sleep(1.1)
+        assert_equal(self.c.get(self.query), None)
+
+    def test_store_retrive(self):
+        self.c.store(self.query, self.response)
+        assert_equal(self.response, self.c.get(self.query) )
