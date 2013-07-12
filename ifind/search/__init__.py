272bd81141de25ccab51a5e3c832cfdde06f0790
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-12 13:37:43 +0100

    change as something isn't working

--- ifind/search/__init__.py
+++ ifind/search/__init__.py
@@ -5,7 +5,3 @@ Search Module API
 from ifind.search.engine import EngineFactory
 from ifind.search.query import Query
 from ifind.search.response import Response
-
-del engine
-del query
-del response
