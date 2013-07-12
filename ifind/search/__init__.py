78920a81cdd05956a42430ebde40901cf43efd29
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-12 13:32:25 +0100

    API PROPERLY DONE NOW - USE 'from ifind.search import <Name>' please

---
+++ ifind/search/__init__.py
@@ -0,0 +1,11 @@
+"""
+Search Module API
+
+"""
+from ifind.search.engine import EngineFactory
+from ifind.search.query import Query
+from ifind.search.response import Response
+
+del engine
+del query
+del response
