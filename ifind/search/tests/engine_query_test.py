3a21c419680879aa811c1cef37815d16b667fc22
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-12 13:01:35 +0100

    usual test sandbox save

--- ifind/search/tests/engine_query_test.py
+++ ifind/search/tests/engine_query_test.py
@@ -1,19 +1,26 @@
 #
 from ifind.search.engine import EngineFactory
 from ifind.search.query import Query
-import pprint
-import json
-# from ifind.search.cache import *
+from ifind.search.response import Response
+import random, string
 #
 #
-engine = EngineFactory('twitter')
+engine = EngineFactory('twitter', cache_type='engine')
+engine2 = EngineFactory('twitter', cache_type='engine')
 #
 #
 query3 = Query("court", top=5, result_type='recent')
 #
 #
 #
-response = engine.search(query3)
-response2 = engine.search(query3)
 
-print response2 == response
+
+def gen_query_response(length=8):
+    query = Query(''.join(random.choice(string.lowercase) for x in xrange(length)))
+    response = Response(query.terms)
+    yield query, response
+
+
+
+
+engine2.search(query3)
