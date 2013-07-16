cebad94a4211a34819f3698e56df117fbed3c69d
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-16 13:10:49 +0100

    lowers result_type upon construction now

--- ifind/search/query.py
+++ ifind/search/query.py
@@ -24,7 +24,7 @@ class Query(object):
 
         """
         self.terms = terms
-        self.result_type = result_type
+        self.result_type = result_type.lower()
         self.lang = lang
         self.top = top
         self.skip = 0
