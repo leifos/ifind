e72a38299323ec431920daa4b97d21dcb8cea901
leif <leifos@acm.org>
2013-07-18 13:14:48 +0100

    Basic HighScoreLeaderboard set up

--- applications/rmiyc_project/leaderboard/views.py
+++ applications/rmiyc_project/leaderboard/views.py
@@ -3,11 +3,13 @@ from django.template import RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
+from ifind.models.game_leaderboards import HighScoresLeaderboard
 
 
 def leaderboards(request):
     context = RequestContext(request, {})
-    return render_to_response('leaderboard/leaderboard.html', context)
+    high_scores = HighScoresLeaderboard().get_leaderboard()
+    return render_to_response('leaderboard/leaderboard.html', {'high_scores':high_scores}, context)
 
 
 
