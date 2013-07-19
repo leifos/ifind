854104b67b2734bfe5fd665ba03721293d139fe4
David Maxwell <maxwelld90@gmail.com>
2013-07-19 12:15:44 +0100

    File uploads can now be served by the Django dev server.

--- applications/rmiyc_project/configuration.py
+++ applications/rmiyc_project/configuration.py
@@ -7,9 +7,9 @@ TEMP_PATH = os.path.join(PROJ_PATH, 'templates')
 STATIC_PATH = os.path.join(PROJ_PATH, 'static/')
 
 MEDIA_PATH = ''
-MEDIA_URL = ''
+MEDIA_URL = '/data/'
 MEDIA_ROOT = 'data/'
-UPLOAD_DIR =  'upload/imgs/'
+UPLOAD_DIR =  '%Y/%m/%d'
 DATA_DIR = os.path.join(PROJ_PATH, 'data')
 
 DEPLOY = False
