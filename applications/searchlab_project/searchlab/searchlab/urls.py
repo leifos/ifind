14bf3ff93ae005291c49cba84d8dc1013a63f7e0
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-24 12:28:58 +0100

    activated admin, created default view for index.html

--- applications/searchlab_project/searchlab/searchlab/urls.py
+++ applications/searchlab_project/searchlab/searchlab/urls.py
@@ -1,17 +1,21 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
+from django.conf.urls import include
+from django.conf.urls import patterns
+from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
+
+)
+
+
+
     # Examples:
     # url(r'^$', 'searchlab_project.views.home', name='home'),
     # url(r'^searchlab_project/', include('searchlab_project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
