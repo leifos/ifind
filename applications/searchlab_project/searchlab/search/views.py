ff82946fb970491612d1b5c5e9b0d8bf877c0324
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-26 14:10:05 +0100

    created ajax search view

--- applications/searchlab_project/searchlab/search/views.py
+++ applications/searchlab_project/searchlab/search/views.py
@@ -1,7 +1,3 @@
-import json
-import string
-
-from django.views.generic.list import ListView
 from django.http import HttpResponse
 
 from .utils import check_input
@@ -9,25 +5,13 @@ from .utils import check_input
 from ifind.search import Query
 from ifind.search import EngineFactory
 
-#
-# class ResultListView(ListView):
-#     """
-#     Generic view to render a list of search results.
-#
-#     """
-#     template_name = 'index.html'
-#     context_object_name = 'result_list'
-#
-#     def get_queryset(self):
-#
-#         engine = EngineFactory('govuk')
-#         query = Query(self.request.REQUEST['q'], top=10)
-#
-#         return engine.search(query).results
-
 
 def search(request):
+    """
+    Accepts GET request containing query terms,
+    searches and returns results as JSON.
 
+    """
     # invalid HTTP method
     if request.method != 'GET':
         return HttpResponse(status=405)
