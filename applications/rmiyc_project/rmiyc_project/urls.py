545c689087f3abfdbdea50eb672292c911f67b60
arazzouk <abdullah.rafik.razzouk@gmail.com>
2013-07-26 16:13:31 +0100

    Merge branch 'master' of https://github.com/leifos/ifind
    Conflicts:
    	applications/rmiyc_project/populate_test_db.py

--- applications/rmiyc_project/rmiyc_project/urls.py
+++ applications/rmiyc_project/rmiyc_project/urls.py
@@ -8,18 +8,15 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'rmiyc_project.views.home', name='home'),
     # url(r'^rmiyc_project/', include('rmiyc_project.foo.urls')),
+    url(r'^users/', include('profiles.urls')),
     url(r'^rmiyc/', include('rmiyc.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^data/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': False}),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^users/', include('rmiyc.urls')),
     url(r'^leaderboard/', include('leaderboard.urls')),
-    #url(r'^users/(?P<username>\w+)', include('profiles.urls'), name='profile_page'),
     url(r'^profile/', include('profiles.urls')),
     url(r'', include('rmiyc.urls')),
 )
