6892c4d4db01ec585438e98cc111395b65996f2a
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-26 14:09:55 +0100

    created ajax search view

--- applications/searchlab_project/searchlab/search/views.py
+++ applications/searchlab_project/searchlab/search/views.py
@@ -1,24 +1,50 @@
+import json
+import string
+
 from django.views.generic.list import ListView
 from django.http import HttpResponse
+
+from .utils import check_input
 
 from ifind.search import Query
 from ifind.search import EngineFactory
 
+#
+# class ResultListView(ListView):
+#     """
+#     Generic view to render a list of search results.
+#
+#     """
+#     template_name = 'index.html'
+#     context_object_name = 'result_list'
+#
+#     def get_queryset(self):
+#
+#         engine = EngineFactory('govuk')
+#         query = Query(self.request.REQUEST['q'], top=10)
+#
+#         return engine.search(query).results
 
-class ResultListView(ListView):
-    """
-    Generic view to render a list of search results.
 
-    """
-    template_name = 'index.html'
-    context_object_name = 'result_list'
+def search(request):
 
-    def get_queryset(self):
+    # invalid HTTP method
+    if request.method != 'GET':
+        return HttpResponse(status=405)
 
-        engine = EngineFactory('govuk')
-        query = Query(self.request.REQUEST['q'], top=10)
+    # checks input for validity
+    query_terms = check_input(request.GET.get('q', ""))
 
-        return engine.search(query).results
+    # bad request
+    if not query_terms:
+        return HttpResponse(status=400)
+
+    # execute query
+    engine = EngineFactory('govuk')
+    query = Query(query_terms)
+    response = engine.search(query)
+
+    return HttpResponse(response.to_json(), content_type='application/json')
 
 
 
