f9d16d9a1975ae376f1d10f7baada0dad9dd7e12
Andrew Gardner <0405116g@student.gla.ac.uk>
2013-07-29 17:16:53 +0100

    rolled my own client-side pagination logic

--- applications/searchlab_project/searchlab/static/js/searchlab.js
+++ applications/searchlab_project/searchlab/static/js/searchlab.js
@@ -1,3 +1,17 @@
+var MAX_PAGE_SIZE = 13
+
+window.pagination =
+{
+    results : null,
+    pages : null,
+    page : {},
+    currentPage : 1
+}
+
+
+$('#pagination-container').hide();
+$('#results-container').hide();
+
 /* Override default behaviour of when 'Enter'
  * key pressed. Makes search request instead.
  * */
@@ -32,28 +46,82 @@ function searchRequest(query_terms)
         type: "GET",
         url: "/search/",
         data: {'q' : query_terms},
-        success: displayResults,
-        error: clearResults
+        success: paginateResults
     });
 }
+
+function paginateResults(data)
+{
+    // store search results
+    var results = data["results"];
+
+    // store results in pagination namespace
+    window.pagination.results = results;
+
+    // use integer division to determine minimum number of pages
+    var pages = ~~(results.length / MAX_PAGE_SIZE);
+
+    // store minimum number of pages in namespace
+    window.pagination.pages = pages;
+
+    // if 0 then need at least one page
+    if (pages == 0) {
+        window.pagination.pages = 1
+    }
+
+    // add extra page for remainder results
+    if (results.length % MAX_PAGE_SIZE != 0) {
+        if (results.length > 5) {
+            window.pagination.pages += 1;
+        }
+    }
+
+    // populate page lookup hash
+    var start = 0;
+    var end = MAX_PAGE_SIZE;
+
+    for (var i=0; i<window.pagination.pages; i++) {
+        window.pagination.page[i+1] = results.slice(start, end);
+        start += MAX_PAGE_SIZE;
+        end += MAX_PAGE_SIZE;
+    }
+
+    window.pagination.currentPage = 1;
+
+    displayResults(window.pagination.page[1]);
+}
+
 
 
 /* Writes results to the appropriate results
  * div.
  * */
-function displayResults(data)
+function displayResults(results)
 {
+    $('#pagination-middle').html(window.pagination.currentPage + " of " + window.pagination.pages);
+
     clearResults();
 
     var resultDiv = $('#result-list');
-    var results = data["results"];
 
-    if (results == 0) {
+    if (results.length == 0) {
+        $('#pagination-container').hide();
         resultDiv.append("No results found.")
     }
 
-    for (var i=0; i<results.length; i++) {
-        resultDiv.append(results[i]['url'] + '<br />');
+    var currentPage = window.pagination.currentPage;
+
+    if (currentPage == 1) {
+        for (var i=0; i<results.length; i++) {
+            resultDiv.append(i+1 + " " + results[i]['url'] + '<br />');
+        }
+    }
+
+    if (currentPage > 1) {
+        var index = (currentPage * MAX_PAGE_SIZE) - (MAX_PAGE_SIZE - 1)
+        for (var i=0; i<results.length; i++, index++) {
+            resultDiv.append(index + " " + results[i]['url'] + '<br />');
+        }
     }
 }
 
@@ -63,4 +131,40 @@ function displayResults(data)
 function clearResults()
 {
     $('#result-list').empty();
+    $('#pagination-container').show();
+    $('#results-container').show();
 }
+
+
+$('#first').click(function(event)
+{
+    window.pagination.currentPage = 1;
+    displayResults(window.pagination.page[1]);
+});
+
+$('#previous').click(function(event)
+{
+    var currentPage = window.pagination.currentPage;
+    if (currentPage > 1) {
+        currentPage -= 1;
+        window.pagination.currentPage = currentPage;
+        displayResults(window.pagination.page[currentPage]);
+    }
+});
+
+$('#next').click(function(event)
+{
+    var currentPage = window.pagination.currentPage;
+    if (currentPage < window.pagination.pages) {
+        currentPage += 1;
+        window.pagination.currentPage = currentPage;
+        displayResults(window.pagination.page[currentPage]);
+    }
+});
+
+$('#last').click(function(event)
+{
+    var final_page = window.pagination.pages;
+    window.pagination.currentPage = final_page;
+    displayResults(window.pagination.page[final_page]);
+});
