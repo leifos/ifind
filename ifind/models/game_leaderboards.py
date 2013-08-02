215d33a9c4378aafa4f08961d7f1754741aaa585
mtbvc <1006404b@student.gla.ac.uk>
2013-08-02 18:35:05 +0100

    Added age group Highscores Leaderboard.

--- ifind/models/game_leaderboards.py
+++ ifind/models/game_leaderboards.py
@@ -92,7 +92,6 @@ class SchoolLeaderboard(GameLeaderboard):
             viewer_school = viewer_profile.school
             #get all viewer's school mates
             profiles = UserProfile.objects.filter(school=viewer_school)
-
             hs = []
             score_list = []
             for profile in profiles:
@@ -102,11 +101,45 @@ class SchoolLeaderboard(GameLeaderboard):
                     Sum += item.highest_score
                 dummy_hs = HighScore(user=profile.user, highest_score=Sum, category=None)
                 score_list.append(dummy_hs)
-
             #hs.order_by('-highest_score')[:self.top]
         #hs = HighScore.objects.all().order_by('-highest_score')[:self.top]
             return self.highscore_to_list(score_list)
 
     def __str__(self):
         return 'School High Scores'
+
+
+
+
+
+
+
+
+
+
+
+class AgeLeaderboard(GameLeaderboard):
+
+    def get_leaderboard(self, user):
+        usr = User.objects.get(username=user.username)
+        if usr.username != '':
+            viewer_profile = UserProfile.objects.get(user=usr)
+            viewer_age = viewer_profile.age
+            #get all in user's age group
+            profiles = UserProfile.objects.filter(age=viewer_age)
+            hs = []
+            score_list = []
+            for profile in profiles:
+                hs = HighScore.objects.filter(user=viewer_profile.user)
+                Sum=0
+                for item in hs:
+                    Sum += item.highest_score
+                dummy_hs = HighScore(user=profile.user, highest_score=Sum, category=None)
+                score_list.append(dummy_hs)
+            #hs.order_by('-highest_score')[:self.top]
+        #hs = HighScore.objects.all().order_by('-highest_score')[:self.top]
+            return self.highscore_to_list(score_list)
+
+    def __str__(self):
+        return 'High Scores by age group'
 
