4ecf280ac180cee723f6ec7bc2510192278103ab
mtbvc <1006404b@student.gla.ac.uk>
2013-08-21 22:06:10 +0100

    Fixed urls regex to escape dots in user names.

--- applications/rmiyc_project/profiles/urls.py
+++ applications/rmiyc_project/profiles/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-        url(r'^(?P<username>\w+)/edit_profile', views.edit_profile),
+        url(r'^(?P<username>\w+\.\w+)/edit_profile', views.edit_profile),
         url(r'^(?P<username>\w+)/graphs.html', views.graphs),
-        url(r'^(?P<username>\w+)', views.profile_page, name='profile'),
+        url(r'^(?P<username>\w+\.\w+)', views.profile_page, name='profile'),
 
       )
