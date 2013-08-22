a3d19e0d4487fe13330aabbee4fcae29b4421625
arazzouk <abdullah.rafik.razzouk@gmail.com>
2013-08-22 19:44:53 +0100

    Analyze

--- applications/rmiyc_project/Analyze.py
+++ applications/rmiyc_project/Analyze.py
@@ -1,28 +1,47 @@
-__author__ = 'arazzouk'
-from ifind.models.game_models import Page ,Category, UserProfile
-import os
-import sys
-sys.path.append(os.getcwd())
 
 
 def get_findability_score():
+    from ifind.models.game_models import Page ,Category, UserProfile
     pages = Page.objects.all()
     f = open('findability.txt', 'a')
     for p in pages:
         if p.no_of_queries_issued != 0:
-            finability = p.no_times_retrieved / p.no_of_queries_issued
+            finability = float(p.no_times_retrieved) / float(p.no_of_queries_issued)
             url = p.url
-            f.write('%d,%s\n' % (finability, url))
+            f.write('%f,%s\n' % (finability, url))
 
 
 def calculate_game_played_no():
-    pass
+    from ifind.models.game_models import Page ,Category, UserProfile
+    profiles = UserProfile.objects.all()
+    countries_list ={}
+    for p in profiles:
+        if p.country:
+            cntry = unicode(p.country.name)
+            if cntry not in countries_list:
+                countries_list[cntry] = 1
+            else:
+                countries_list[cntry] += 1
+    for key in countries_list:
+        print key
+        print countries_list[key]
 
 
 def get_players_ages():
-    pass
+    from ifind.models.game_models import Page ,Category, UserProfile
+    profiles = UserProfile.objects.all()
+    age_list ={}
+    for p in profiles:
+        if p.age:
+            if p.age not in age_list:
+                age_list[p.age] = 1
+            else:
+                age_list[p.age] += 1
+    for key in age_list:
+        print key
+        print age_list[key]
 
 
 
 
-get_findability_score()
+get_players_ages()
