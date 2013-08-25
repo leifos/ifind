8972fc553568aca68f98ae30cc1ce1754252069f
leif <leifos@acm.org>
2013-08-25 15:07:30 +0100

    minor updates to asg game

--- applications/asg_project/asg/urls.py
+++ applications/asg_project/asg/urls.py
@@ -6,6 +6,7 @@ import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^start/', views.startgame, name='startgame'),
+        url(r'^start2/', views.startgame2, name='startgame2'),
         url(r'^query/$', views.query, name='query'),
         url(r'^assess/$', views.assess, name='assess'),
 
--- applications/asg_project/asg/views.py
+++ applications/asg_project/asg/views.py
@@ -5,10 +5,12 @@ from django.template import RequestContext
 
 from ifind.search.cache import RedisConn
 from ifind.asg.abstract_search_game import ABSGame
-from ifind.asg.asg_generator import RandomYieldGenerator, CueGenerator
+from ifind.asg.asg_generator import RandomYieldGenerator, ConstantLinearYieldGenerator, TestYieldGenerator, CueGenerator
 
 ryg = RandomYieldGenerator()
-cg = CueGenerator()
+tyg = TestYieldGenerator()
+cyg = ConstantLinearYieldGenerator()
+cg = CueGenerator(cue_length=30)
 
 
 def index(request):
@@ -22,13 +24,29 @@ def startgame(request):
         print session_id
         rc = RedisConn()
         rc.connect()
-        game = ABSGame(ryg,cg)
+        game = ABSGame(ryg,cg,cq=2)
         game.start_game()
         data = game.get_game_state()
         rc.store(session_id, game)
         response = render_to_response('asg/game.html', {'sid':session_id, 'data': data }, context)
         response.set_cookie('gid',session_id)
         return response
+
+def startgame2(request):
+        context = RequestContext(request, {})
+        request.session.flush()
+        session_id = request.session._get_or_create_session_key()
+        print session_id
+        rc = RedisConn()
+        rc.connect()
+        game = ABSGame(tyg,cg,cq=2)
+        game.start_game()
+        data = game.get_game_state()
+        rc.store(session_id, game)
+        response = render_to_response('asg/game.html', {'sid':session_id, 'data': data }, context)
+        response.set_cookie('gid',session_id)
+        return response
+
 
 def query(request):
         context = RequestContext(request, {})
