ebef648b38097d1747f1999a46ff8f5bb8fc77b8
leif <leifos@acm.org>
2013-09-04 10:19:07 +0100

    Added Leaderboard to asg_project

--- applications/asg_project/asg/urls.py
+++ applications/asg_project/asg/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^start/(?P<num>[0-9]+)/$', views.startgame, name='startgame'),
         url(r'^pick/', views.pick, name='pick'),
-
         url(r'^query/$', views.query, name='query'),
         url(r'^assess/$', views.assess, name='assess'),
-        url(r'^(?P<username>[a-zA-Z0-9_.+@-]+)', views.profile_page, name='profile'),
+        url(r'^players/(?P<username>[a-zA-Z0-9_.+@-]+)', views.profile_page, name='profile'),
+        url(r'^leaderboard/$', views.leaderboard, name='leaderboard'),
 )
