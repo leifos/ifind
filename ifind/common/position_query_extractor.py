24b4b11eaa0925a0a136aa764665a83b8db328e5
lysethia <dropofjupiter@hotmail.com>
2013-10-04 14:32:40 +0100

    refactoring of pqe

--- ifind/common/position_query_extractor.py
+++ ifind/common/position_query_extractor.py
@@ -6,13 +6,17 @@ from query_generation import BiTermQueryGeneration
 from language_model import LanguageModel
 
 
-class PositionQueryExtractor():
+class PositionQueryExtractor(object):
 
-    def __init__(self, html):
+    def __init__(self, html, div_ids):
         self.html = html
         self.xml_tree = ElementTree.fromstring(self.html)
+        self.div_ids=div_ids
 
-    def remove_div_content(self, div_ids):
+    def set_div_ids(self, ids):
+        self.div_ids = ids
+
+    def remove_div_content(self):
         """
             returns a string with the content the html with the content of
             divs in div_ids removed
@@ -21,7 +25,7 @@ class PositionQueryExtractor():
             """
         result = ''
         tree_copy = deepcopy(self.xml_tree)
-        for div_id in div_ids:
+        for div_id in self.div_ids:
             #need to check the children of the root and then their children
             for element in tree_copy:
                 #if the child is a div with the target div id then use the parent to
@@ -43,7 +47,7 @@ class PositionQueryExtractor():
         return result
 
 
-    def get_subtext(self, text, num_words):
+    def get_subtext(self, text, num_words=0, percentage=None):
         """
             takes first num_words from text and return them as a string
             :param text:
@@ -51,13 +55,23 @@ class PositionQueryExtractor():
             """
         words = text.split()
         subtext = ' '
-        if len(words) > num_words:
-            return subtext.join(words[0:num_words])
-            # for term in :
-            #     print term
-            #     subtext += ' '.join(term)
+        if(percentage):
+            num_words = round(self.calc_percentage(percentage,len(words)))
+        if(num_words):
+            if len(words) > num_words:
+                return subtext.join(words[0:num_words])
+                # for term in :
+                #     print term
+                #     subtext += ' '.join(term)
+            else:
+                return text
+
+
+    def calc_percentage(self, percentage, total_words):
+        if total_words == 0:
+            return 0
         else:
-            return text
+            return 100 * float(percentage)/float(total_words)
 
 
     def generate_queries(self, text):
@@ -65,7 +79,7 @@ class PositionQueryExtractor():
             cleans and generates queries from the text passed in
             :return: list of text queries
             """
-        print "text is ", text
+        #print "text is ", text
         bi_gen = BiTermQueryGeneration()
         query_list = bi_gen.extract_queries_from_text(text)
 
--- ifind/common/test_position_extractor.py
+++ ifind/common/test_position_extractor.py
@@ -12,36 +12,42 @@ class TestPositionExtractor(unittest.TestCase):
                '</div><div id="content"><p>this is important</p>' \
                '<p> study computing it is fun</p></div>' \
                '<div id="footer"> <h2>byes</h2></div> </html> '
-        self.extractor = PositionQueryExtractor(html=html)
+        div_ids=[]
+        self.extractor = PositionQueryExtractor(html=html, div_ids=div_ids)
 
     def test_remove_div_content(self):
+
         div_id=["header"]
+        self.extractor.set_div_ids(div_id)
         expected = "this is important study computing it is fun byes"
-        result = self.extractor.remove_div_content(div_id)
+        result = self.extractor.remove_div_content()
         self.process_test_equals(expected,result)
         div_id=["content"]
         expected = "hello world byes"
-        result = self.extractor.remove_div_content(div_id)
+        self.extractor.set_div_ids(div_id)
+        result = self.extractor.remove_div_content()
         self.process_test_equals(expected,result)
         #test multiple div removal
         ignore_divs = ['header','footer']
-        result = self.extractor.remove_div_content(ignore_divs)
+        self.extractor.set_div_ids(ignore_divs)
+        result = self.extractor.remove_div_content()
         expected = 'this is important study computing it is fun'
         self.process_test_equals(expected, result)
 
     def test_get_subtext(self):
         text = "this is a sentence which has some words in it"
-        result = self.extractor.get_subtext(text,2)
+        result = self.extractor.get_subtext(text,num_words=2)
         expected = "this is"
         self.process_test_equals(expected,result)
         #test greater than length returns whole text
-        result = self.extractor.get_subtext(text, 12)
+        result = self.extractor.get_subtext(text, num_words=12)
         self.process_test_equals(text, result)
 
     def test_generate_queries(self):
         #test removing div content and generating queries from result
         expected = ['this important','important study','study computing','computing fun']
-        main_text = self.extractor.remove_div_content(["header","footer"])
+        self.extractor.set_div_ids(["header","footer"])
+        main_text = self.extractor.remove_div_content()
         result = self.extractor.generate_queries(main_text)
         self.process_test_equals(expected, result)
         #test removing div content, and then using only first 4 words
