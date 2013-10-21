42a459dc41cee9ea48f7e332c022643102e05355
mtbvc <1006404b@student.gla.ac.uk>
2013-10-21 02:19:01 +0100

    Change to upload paths for images.

--- applications/pagefetch_project/configuration.py
+++ applications/pagefetch_project/configuration.py
@@ -6,9 +6,11 @@ print "Project Path: %s" % (PROJ_PATH)
 GAME_DB = os.path.join(PROJ_PATH,'game.db')
 TEMP_PATH = os.path.join(PROJ_PATH,'templates')
 STATIC_PATH = os.path.join(PROJ_PATH,'static')
-MEDIA_PATH = os.path.join(PROJ_PATH,'media')
-MEDIA_URL  = '/media/'
-UPLOAD_DIR = os.path.join(PROJ_PATH,'media')
+MEDIA_PATH = os.path.join('')
+MEDIA_URL  = '/data/'
+#UPLOAD_DIR = os.path.join(PROJ_PATH,'data')
+UPLOAD_DIR =  '%Y/%m/%d'
+
 MEDIA_ROOT = 'data/'
 
 DEPLOY = False
