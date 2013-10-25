1d24f952d6b4030c91cc15533331dcf23a6e6008
leif <leifos@acm.org>
2013-10-25 17:17:51 +0100

    updated treconomics urls

--- applications/treconomics_project/treconomics_project/urls.py
+++ applications/treconomics_project/treconomics_project/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^treconomics/', include('treconomics.urls')),
+    url(r'', include('treconomics.urls')),
 )
 
 
