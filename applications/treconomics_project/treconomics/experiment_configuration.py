84e23f351575d5bb777193019fdc7f92f6d03e10
leif <leifos@acm.org>
2013-10-25 15:15:54 +0100

    Tidying up exp config

--- applications/treconomics_project/treconomics/experiment_configuration.py
+++ applications/treconomics_project/treconomics/experiment_configuration.py
@@ -16,6 +16,13 @@ print "Work DIR: " + work_dir
 print "QRELS File: " + qrels_file
 print "my_whoosh_doc_index_dir: " + my_whoosh_doc_index_dir
 
+event_logger = logging.getLogger('event_log')
+event_logger.setLevel(logging.INFO)
+event_logger_handler = logging.FileHandler(os.path.join(my_experiment_log_dir, 'experiment.log' ) )
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+event_logger_handler.setFormatter(formatter)
+event_logger.addHandler(event_logger_handler)
+
 # workflow must always start with /treconomics/startexperiment/
 
 exp_work_flows = [
@@ -24,24 +31,7 @@ exp_work_flows = [
 ['/treconomics/startexperiment/','/treconomics/preexperiment/US/','/treconomics/search/1/','/treconomics/search/2/','/treconomics/search/3/','/treconomics/nasaloadsurvey/','/treconomics/nasaqueryloadsurvey/','/treconomics/nasanavigationloadsurvey/','/treconomics/nasaassessmentloadsurvey/','/treconomics/nasafactorcomparesurvey/','/treconomics/performance/','/treconomics/logout/'],
 ['/treconomics/startexperiment/','/treconomics/preexperiment/UK/','/treconomics/search/1/','/treconomics/search/2/','/treconomics/search/3/','/treconomics/nasaloadsurvey/','/treconomics/nasaqueryloadsurvey/','/treconomics/nasanavigationloadsurvey/','/treconomics/nasaassessmentloadsurvey/','/treconomics/performance/','/treconomics/logout/'],
 ['/treconomics/startexperiment/','/treconomics/preexperiment/UK/','/treconomics/search/1/','/treconomics/search/2/','/treconomics/search/3/','/treconomics/performance/','/treconomics/logout/']
-
 ]
-
-# these are the rotations of topics
-# We could put a method here that reads the topics from the database table, and then automatically constructs the rotations here
-
-#rotations = [ [347, 344, 435 ], [347, 435, 344], [344, 347, 435], [344, 435, 347], [435, 347, 344], [435, 344, 347]  ]
-
-# timeout is in seconds
-#timeout = 660
-
-event_logger = logging.getLogger('event_log')
-event_logger.setLevel(logging.INFO)
-event_logger_handler = logging.FileHandler(os.path.join(my_experiment_log_dir, 'experiment.log' ) )
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-event_logger_handler.setFormatter(formatter)
-event_logger.addHandler(event_logger_handler)
-
 
 class ExperimentSetup(object):
 
@@ -88,6 +78,6 @@ exp3 = ExperimentSetup(workflow= exp_work_flows[4], interface=2, description='su
 exp4 = ExperimentSetup(workflow= exp_work_flows[4], rpp=3)
 exp5 = ExperimentSetup(workflow= exp_work_flows[4], rpp=6)
 
-
+# these correspond to conditions
 experiment_setups = [exp0,exp1,exp2,exp3,exp4,exp5]
 
