c43f9dc2757c25e6551caa736059ed7c807a5bf0
David Maxwell <maxwelld90@gmail.com>
2013-10-29 14:34:00 +0000

    Updated popup code for Treconomics - in a seprate JS file, and you can specify the size of the box depending on the Experiment chosen

--- applications/treconomics_project/static/javascripts/jquery_treconomics.js
+++ applications/treconomics_project/static/javascripts/jquery_treconomics.js
@@ -28,7 +28,6 @@ $(document).ready(function()
         });
     });
 
-
 }); 
 
 
--- applications/treconomics_project/treconomics/experiment_functions.py
+++ applications/treconomics_project/treconomics/experiment_functions.py
@@ -25,6 +25,10 @@ def get_experiment_context(request):
     ec["completed_steps"] = profile.steps_completed
     ec["workflow"] = experiment_setups[ec['condition']].workflow
 
+    # Variables determining the width/height of the epxeriment assigned to the user.
+    ec["popup_width"] = experiment_setups[ec['condition']].popup_width
+    ec["popup_height"] = experiment_setups[ec['condition']].popup_height
+
     #print ec["workflow"]
     if profile.data == "test":
         ec["test_user"] = True
@@ -65,6 +69,8 @@ def print_experiment_context(ec):
     print "current step: " + str(ec["current_step"])
     print "taskid: " + str(ec["taskid"])
     print "topicnum: " + str(ec["topicnum"])
+    print "popup width: " + str(ec["popup_width"])
+    print "popup height: " + str(ec["popup_height"])
 
 def time_search_experiment_out(request):
     start_time = request.session['start_time']
