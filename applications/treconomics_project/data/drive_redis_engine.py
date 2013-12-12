d5f953497b46397e63060678087b9a808cec7008
David Maxwell <maxwelld90@gmail.com>
2013-12-12 17:03:11 +0000

    Updated updated engine drive code

--- applications/treconomics_project/data/drive_redis_engine.py
+++ applications/treconomics_project/data/drive_redis_engine.py
@@ -1,28 +1,58 @@
 import os
+import timeit
 from ifind.search.query import Query
 from ifind.search.engines.whooshtrecnewsupdated import WhooshTrecNews
 
-
+# Paths
 work_dir = os.getcwd()
 whoosh_index_dir = os.path.join(work_dir, 'fullindex')
+query_file = os.path.join(work_dir, '347.sigir2013.queries')
 
-query = Query(terms='banana monkey', top=10)
-query.skip=1
-query.top = 10
+# Settings
+skip = 1
+top = 10
+ignore_terms = ['and', 'or', 'not']
+
+# Engine
 engine = WhooshTrecNews(whoosh_index_dir=whoosh_index_dir)
 
-response = engine.search(query)
+def get_query_list():
+	input_file = open(query_file, 'r')
+	query_list = []
+	
+	for line in input_file:
+		line = line.strip()
+		line = line.partition(' ')
+		
+		query = line[2]
+		query = query.strip()
+		query = query.lower()
+		query = query.replace('"', '')
+		
+		query = query.split()
+		new_query = ""
+		
+		for term in query:
+			if term not in ignore_terms:
+				new_query = new_query + term + ' '
+		
+		new_query = new_query.strip()
+		query_list.append(new_query)
+	
+	input_file.close()
+	return query_list
 
-for res in response:
-	print res.docid
+query_list = get_query_list()
 
-
-
-"""
-# Add the total number of pages from the results object as an attribute of our response object.
-# We also add the total number of results shown on the page.
-setattr(response, 'total_pages', results.pagecount)
-setattr(response, 'results_on_page', results.pagelen)
-setattr(response, 'actual_page', results.actual_page)
-return response
-"""
+for query in query_list:
+	query_obj = Query(terms=query, top=top)
+	query_obj.skip = skip
+	
+	start_time = timeit.default_timer()
+	
+	results = engine.search(query_obj)
+	
+	print
+	print "Query '{0}' executed in {1:.2f} seconds".format(query, (timeit.default_timer() - start_time))
+	print "Got {0} result(s)".format(len(results))
+	print
