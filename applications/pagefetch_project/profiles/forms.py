97ef5de42c02fc4d857be814a9883d7ef98ca181
mtbvc <1006404b@student.gla.ac.uk>
2014-02-09 23:45:26 +0000

    removed avatar, school form edit_profile form

--- applications/pagefetch_project/profiles/forms.py
+++ applications/pagefetch_project/profiles/forms.py
@@ -2,13 +2,13 @@ from django.forms import ModelForm
 from ifind.models.game_models import UserProfile, User
 
 
-
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['email']
 
+
 class ProfileForm(ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['profile_pic', 'age', 'gender', 'school', 'city', 'country']
+        fields = ['age', 'gender', 'city', 'country']  # profile_pic + school
