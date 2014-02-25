4ac90e74b2bfc380a8ed088abcb089672c2b4a5c
mtbvc <1006404b@student.gla.ac.uk>
2014-02-25 03:24:20 +0000

    fixed a bunch of bugs in pyanywhere pop script

--- applications/pagefetch_project/pythonanywhere_population.py
+++ applications/pagefetch_project/pythonanywhere_population.py
@@ -10,7 +10,7 @@ Author: mtbvc <1006404b@student.gla.ac.uk>
 Date:   24/02/2014
 """
 #from ifind.models import game_model_functions
-from rmiyc_project import settings
+from pagefetch_project import settings
 from django.core.management import setup_environ
 setup_environ(settings)
 
@@ -28,7 +28,7 @@ def achievements():
 
 
 def players():
-    Jimmy = User(username="Jimmy")
+    jim = User(username="Jimmy")
     jane = User(username="Jane")
     jake = User(username="Jake")
     jim.set_password("test")
@@ -43,24 +43,32 @@ def players():
 
 
 def highscores():
-    HighScore(user=User.objects.filter(username='Jim')[0],category=Category.objects.filter(name="Films")[0],highest_score=2566).save()
-    HighScore(user=User.objects.filter(username='Jim')[0],category=Category.objects.filter(name="Actors")[0],highest_score=3200).save()
+    HighScore(user=User.objects.filter(username='Jimmy')[0],category=Category.objects.filter(name="Films")[0],highest_score=2566).save()
+    HighScore(user=User.objects.filter(username='Jimmy')[0],category=Category.objects.filter(name="Actors")[0],highest_score=3200).save()
     HighScore(user=User.objects.filter(username='Jane')[0],category=Category.objects.filter(name="Films")[0],highest_score=1420).save()
     HighScore(user=User.objects.filter(username='Jake')[0],category=Category.objects.filter(name="Games")[0],highest_score=1667).save()
-    HighScore(user=User.objects.filter(username='Jim')[0],category=Category.objects.filter(name="Universities")[0],highest_score=500).save()
+    HighScore(user=User.objects.filter(username='Jimmy')[0],category=Category.objects.filter(name="Universities")[0],highest_score=500).save()
     HighScore(user=User.objects.filter(username='Jake')[0],category=Category.objects.filter(name="Films")[0],highest_score=179).save()
     HighScore(user=User.objects.filter(username='Jane')[0],category=Category.objects.filter(name="Actors")[0],highest_score=800).save()
     HighScore(user=User.objects.filter(username='Jake')[0],category=Category.objects.filter(name="Actors")[0],highest_score=700).save()
 
 
 def player_achievements():
-    jim = User.objects.filter(username='Jim')
+    jim = User.objects.filter(username='Jimmy')
     ac2 = Achievement.objects.filter(name="UberSearcher")
     PlayerAchievement(user=jim[0], achievement=ac2[0]).save()
 
 
 def main():
     achievements()
+    print("populated achievements")
     players()
+    print ("populated players")
     highscores()
+    print("populated hs")
     player_achievements()
+    print("populated pl_achievements")
+
+
+if __name__ == '__main__':
+    main()
