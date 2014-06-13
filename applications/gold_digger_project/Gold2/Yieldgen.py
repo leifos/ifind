081a95f4a5d3db4ed6959b9ee181e68a6a6468ae
Granvanoeli <levent.jade@gmail.com>
2014-06-13 17:18:23 +0100

    functions

--- applications/gold_digger_project/Gold2/Yieldgen.py
+++ applications/gold_digger_project/Gold2/Yieldgen.py
@@ -2,10 +2,10 @@ __author__ = 'gabriele'
 import random
 
 
-def random_yield (depth):
+def random_yield(depth):
     random_yield_array = []
     for index in range(depth):
-        random_yield_array[index] = random.randint(1,10)
+        random_yield_array[index] = random.randint(1, 10)
     return random_yield_array
 
 
@@ -15,8 +15,14 @@ def constant_yield(depth, yield_constant):
         constant_yield_array[index] = yield_constant
     return constant_yield_array
 
+
 def linear_yield(depth):
     linear_yield_array = []
-    for index in range(depth)
+    for index in range(depth):
+        linear_yield_array[index] = linear_function(index)
+    return linear_yield_array
 
 
+def linear_function(x):
+    y = (-0.6*x) + 18  # This is the linear yield function
+    return y
