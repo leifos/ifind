5fe53663d725feea79d204450ee195d138c91a5c
goodbrandcraig <goodbrandcraig@gmail.com>
2014-06-18 11:37:52 +0100

    Created utils.py, created dict of mod functions

---
+++ applications/slowsearch_project/slowsearch/utils.py
@@ -0,0 +1,40 @@
+__author__ = 'Craig'
+
+import time
+from ifind.search import Query, EngineFactory
+from keys import BING_API_KEY
+
+
+def mod_normal(results):
+    return results
+
+
+def mod_slow(results):
+    time.sleep(5)
+    return results
+
+
+def mod_bad(results):
+    return results
+
+conditions = {
+    '1': mod_normal,
+    '2': mod_slow,
+    '3': mod_bad
+}
+
+
+# run a search query on Bing using the query string passed
+def run_query(query, condition='2'):
+    q = Query(query, top=10)
+    e = EngineFactory("Bing", api_key=BING_API_KEY)
+
+    response = e.search(q)
+
+    print 'modding results'
+    mod = conditions[condition]
+    mod_results = mod(response)
+
+    return mod_results
+
+
