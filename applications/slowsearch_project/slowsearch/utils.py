84555f3116e4405b2a1a96ef7049f34a3d409a5e
goodbrandcraig <goodbrandcraig@gmail.com>
2014-06-23 15:37:53 +0100

    set up pagination of search results
    created new function in utils.py, paginated_search, to handle pagination
    of the results. Had to store query as a session for now, not sure if
    this is the best way to do this

--- applications/slowsearch_project/slowsearch/utils.py
+++ applications/slowsearch_project/slowsearch/utils.py
@@ -2,17 +2,20 @@ __author__ = 'Craig'
 
 import time
 from ifind.search import Query, EngineFactory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from keys import BING_API_KEY
 
 e = EngineFactory("Bing", api_key=BING_API_KEY)
+
+delay_time = 5
 
 
 def mod_normal(results):
     return results
 
 
-def mod_slow(results):
-    time.sleep(0)
+def mod_slow(results, delay=delay_time):
+    time.sleep(delay)
     return results
 
 
@@ -27,8 +30,8 @@ conditions = {
 
 
 # run a search query on Bing using the query string passed
-def run_query(query, condition=2):
-    q = Query(query, top=10)
+def run_query(query, condition=1):
+    q = Query(query, top=100)
 
     response = e.search(q)
 
@@ -36,5 +39,26 @@ def run_query(query, condition=2):
     mod_results = mod(response)
 
     return mod_results
+
+
+def paginated_search(request, query):
+    if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+            paginator = Paginator(result_list.results, 10)  # show 10 results per page
+
+            page = request.REQUEST.get('page')
+
+            try:
+                contacts = paginator.page(page)
+            except PageNotAnInteger:
+                # if page not an integer, deliver first page
+                contacts = paginator.page(1)
+            except EmptyPage:
+                # if page out of range, deliver last page of results
+                contacts = paginator.page(paginator.num_pages)
+
+            return contacts
 
 
