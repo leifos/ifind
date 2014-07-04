89356d803b3f796ff62a5aa67d57f8c88b0e1894
Iain <ih01@Leif-Azzopardis-MacBook-Pro-15.local>
2014-07-04 14:34:12 +0100

    styling

--- applications/badsearch_project/badsearch/forms.py
+++ applications/badsearch_project/badsearch/forms.py
@@ -17,10 +17,10 @@ EXPERIENCE_CHOICES = (('A', 'Agree'), ('U', 'Unsure'), ('D', 'Disagree'))
 
 
 class UserForm(forms.ModelForm):
-    username = forms.CharField(help_text="Please enter a username.")
-    email = forms.CharField(help_text="Please enter your email.")
-    password = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput(),label='Repeat Password')
+    username = forms.CharField(label="Username*")
+    email = forms.CharField(label="Email*")
+    password = forms.CharField(label="Password*",widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput(),label='Repeat Password*')
 
     def clean(self):
         password = self.cleaned_data.get('password')
@@ -44,8 +44,8 @@ class UserProfileForm(forms.ModelForm):
 
 
 class DemographicsForm(forms.ModelForm):
-    age = forms.IntegerField(label="Please provide your age (in years).", max_value=100, min_value=0, required=False)
-    sex = forms.CharField(max_length=20, widget=forms.Select(choices=GENDER_CHOICES), label="Please indicate your sex.",
+    age = forms.IntegerField(label="Please provide your age (in years)", max_value=100, min_value=0, required=False)
+    sex = forms.CharField(max_length=20, widget=forms.Select(choices=GENDER_CHOICES), label="Please indicate your sex",
                           required=False)
     education_undergrad = forms.CharField(widget=forms.Select(choices=YES_CHOICES),
                                           label="Are you undertaking, or have you obtained, an undergraduate degree?",
@@ -68,22 +68,22 @@ class DemographicsForm(forms.ModelForm):
 
 class ValidationForm(forms.Form):
     terms = forms.BooleanField(required=True, initial=False,
-                               label="I have read and agree to the above Study Information and I have had the opportunity to ask any questions",
+                               label="I have read and agree to the above Study Information and I have had the opportunity to ask any questions:",
                                error_messages={'required': 'You must accept the terms and conditions'}, )
     questions = forms.BooleanField(required=True, initial=False,
-                               label="I understand that I am able to ask questions about this study at any time",
+                               label="I understand that I am able to ask questions about this study at any time:",
                                error_messages={'required': 'You must accept the terms and conditions'}, )
     name = forms.BooleanField(required=True, initial=False,
-                               label="I understand that my name will not appear in any published document relating to research conducted as part of this study.",
+                               label="I understand that my name will not appear in any published document relating to research conducted as part of this study:",
                                error_messages={'required': 'You must accept the terms and conditions'}, )
     info = forms.BooleanField(required=True, initial=False,
-                               label="I am willing for anonymous data from my search sessions and questionnaires that I have submitted may be quoted in papers, journal articles and books that may be written by the researchers.",
+                               label="I am willing for anonymous data from my search sessions and questionnaires that I have submitted may be quoted in papers, journal articles and books that may be written by the researchers:",
                                error_messages={'required': 'You must accept the terms and conditions'}, )
     agree = forms.BooleanField(required=True, initial=False,
-                               label="I agree to take part in this study",
+                               label="I agree to take part in this study:",
                                error_messages={'required': 'You must accept the terms and conditions'}, )
     notify = forms.BooleanField(required=False, initial=False,
-                               label="I would like to be notified by email of the outcome of this experiment",
+                               label="I would like to be notified by email of the outcome of this experiment:",
                                error_messages={'required': 'You must accept the terms and conditions'}, )
 
 class FinalQuestionnaireForm(forms.ModelForm):
