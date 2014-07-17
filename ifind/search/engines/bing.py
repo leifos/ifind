fee03a4fb5658678cc436a9136fec72429c67236
goodbrandcraig <goodbrandcraig@gmail.com>
2014-07-17 18:08:17 +0100

    URLs now ranked by ifind code and rank is logged
    Can now edit profile page properly, removed slowsearch from URLs and
    links

--- ifind/search/engines/bing.py
+++ ifind/search/engines/bing.py
@@ -226,11 +226,17 @@ class Bing(Engine):
         response = Response(query.terms)
 
         content = json.loads(results.text)
+		
+        rank_counter = 1
 
         if query.result_type == 'web' or not query.result_type:
             for result in content[u'd'][u'results'][0][u'Web']:
-                response.add_result(title=result[u'Title'], url=result[u'Url'], summary=result[u'Description'])
-
+                response.add_result(title=result[u'Title'], url=result[u'Url'], summary=result[u'Description'], rank=rank_counter)
+                print result[u'Title']
+                print rank_counter
+                print ' '
+                rank_counter+=1
+				
         if query.result_type == 'image':
             for result in content[u'd'][u'results'][0][u'Image']:
                 file_size = str(int(result[u'FileSize']) / 1024)  # in kilobytes
