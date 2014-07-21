eb9487dae5ae9fd49f025b4b907182ba3b334364
Granvanoeli <levent.jade@gmail.com>
2014-07-21 01:50:38 +0100

    ajax 2

--- applications/gold_digger_project/static/js/dig-ajax.js
+++ applications/gold_digger_project/static/js/dig-ajax.js
@@ -1,14 +1,24 @@
 /**
  * Created by gabriele on 16/07/14.
  */
+
+var count = 0;
 $(document).ready(function(){
-    $('#digbutton').click(function(){
-        var pos = $('#blockposition').val();
-        var gold = $('#digbutton').val();
+    console.log("Ajax started");
+
+    $('.buttons').click(function(){
+
+        var pos = $('#blockposition_'+count).val();
+        var gold = $('#digbutton_'+count).val();
 
         var csrf = $('#csrf > input').val();
-        $('#row_'+pos).addClass("hidden");
 
+        $('#invisiblebuttons_'+count).addClass("hidden");
+
+        count += 1;
+        console.log(count);
+        console.log(pos);
+        console.log(gold);
         $.ajax({
             type: "POST",
             url: "/gold_digger/ajaxview/",
@@ -16,16 +26,17 @@ $(document).ready(function(){
             success: function(response){
 
 
-                console.log(pos);
+
+                console.log(count);
                 $('#totalgold').html(response['totalgold']);
                 $('#progressbar').css("width", response['timeremaining']);
                 $('#currentgold').html(response['currentgold']);
 
                 $('#goldlayer').removeClass().addClass("row nuggets_"+response['nuggets']);
                 $('#scaffoldlayer').removeClass().addClass("scaffold_"+Math.floor((Math.random() * 3) + 1));
-                $('#row_1').removeClass()
+                $('#invisiblebuttons_'+count).removeClass("hidden");
 
             }
         })
     })
-})
+});
