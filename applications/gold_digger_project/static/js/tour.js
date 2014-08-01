5dc57f584eeeb4a2bb17d5b367da73e9d2a3a0c4
Granvanoeli <levent.jade@gmail.com>
2014-08-01 18:55:00 +0100

    prices and tour

--- applications/gold_digger_project/static/js/tour.js
+++ applications/gold_digger_project/static/js/tour.js
@@ -1,16 +1,3 @@
-console.log("Tour");
-var tour = new Tour({
-  steps: [
-  {
-    element: ".provatour",
-    title: "Prova",
-    content: "This is a test"
-  }
-
-]});
-
-// Initialize the tour
-tour.init();
-
-// Start the tour
-tour.start();
+$(document).ready(function(){
+introJs().start();
+});
