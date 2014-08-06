f80af7761083fc0fd414a380ec5792db07932a84
goodbrandcraig <goodbrandcraig@gmail.com>
2014-08-06 10:31:39 +0100

    improved profile page

--- applications/slowsearch_project/slowsearch_project/settings.py
+++ applications/slowsearch_project/slowsearch_project/settings.py
@@ -135,7 +135,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'slowsearch'
+    'slowsearch',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
