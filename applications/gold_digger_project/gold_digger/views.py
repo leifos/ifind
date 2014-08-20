608c580feed8b9fb74150d11f2b84429415608b1
Granvanoeli <levent.jade@gmail.com>
2014-08-20 11:23:24 +0100

    stuff

--- applications/gold_digger_project/gold_digger/views.py
+++ applications/gold_digger_project/gold_digger/views.py
@@ -147,7 +147,7 @@ def user_login(request):
                 login(request, user)
                 request.session['time_remaining'] = 100
                 request.session['gold'] = 0
-                request.session['mine_no'] =0
+                request.session['mine_no'] = 0
                 request.session['days'] = 1
 
                 event_logger.info('USER ' + username + ' LOGIN')
@@ -365,6 +365,12 @@ def game2(request):
 
         event_logger.info('USER ' + user.user.username + ' LIFE ' + life_s + ' TOT ' + mines_s + ' DAY ' + days_s + ' LOC ' + mine_type + ' MNO ' + mine_no_s)
 
+        #cumgold = request.session['gold']
+        #digcost = user.tool.time_modifier
+        #move_cost = user.vehicle.modifier
+
+        #event_logger.info('USER ' + user.user.username + ' LIFE ' + life_s + ' TOT ' + mines_s + ' DAY ' + days_s + ' SMOVE ')
+
     if not request.session['has_mine']:
         gen = yieldgen.YieldGenerator
 
@@ -419,8 +425,10 @@ def game2(request):
         real_array = []
 
         for b in blocks:
-            a = round(b.gold*user.tool.modifier, 1)
+            a = round(b.gold*user.tool.modifier)
             real_array.append(a)
+
+        should_stop(real_array)
 
         real_array_s = str(real_array)
 
@@ -1028,3 +1036,12 @@ def egg(request):
 
     else:
         return HttpResponse(status=204)
+
+def should_stop(real_array):
+
+    stopmax  = 0
+    for r in real_array:
+        if r > stopmax:
+            stopmax = r
+
+    return stopmax
