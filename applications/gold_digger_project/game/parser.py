843495f50438577ac4dea5be78191793c55977eb
Granvanoeli <levent.jade@gmail.com>
2014-09-04 19:57:53 +0100

    new data

--- applications/gold_digger_project/game/parser.py
+++ applications/gold_digger_project/game/parser.py
@@ -1,8 +1,43 @@
 __author__ = '2108535R'
 
-file = open('../clean_log', 'r')
-file_a = open('../clean_log_ordered', 'a')
+### PARSER V.1 ############################################################
 
+# file = open('../clean_log', 'r')
+# file_a = open('../clean_log_ordered', 'a')
+#
+#
+# switch = True
+#
+# for line in file:
+#     split_log_line = line.split(" ")
+#     index = 0
+#     ordered_line = []
+#
+#     for s in split_log_line:
+#         if s == 'SMOVE' and switch == True:
+#             beta_i = int(split_log_line[18])/int(split_log_line[16])
+#             beta = str(beta_i)
+#
+#             file_a.write(split_log_line[4].rstrip('\n') + " ")
+#             file_a.write(split_log_line[index+1].rstrip('\n') + " ")
+#             file_a.write(split_log_line[16].rstrip('\n') + " ")
+#             file_a.write(split_log_line[18].rstrip('\n') + " ")
+#             file_a.write(beta.rstrip('\n') + " ")
+#
+#
+#             switch = False
+#
+#         if s == 'MOVE' and switch == False:
+#             file_a.write(split_log_line[index+1])
+#             switch = True
+#
+#         index += 1
+
+### PARSER V.2 ############################################################
+
+file = open('../log_NLOC2', 'r')
+file_a = open('../ordered_log_NLOC', 'a')
+loc = ''
 
 switch = True
 
@@ -11,8 +46,13 @@ for line in file:
     index = 0
     ordered_line = []
 
+
     for s in split_log_line:
-        if s == 'SMOVE' and switch == True:
+
+        if s == 'SCAN':
+            loc = split_log_line[index+1]
+
+        if s == 'SMOVE' and switch is True:
             beta_i = int(split_log_line[18])/int(split_log_line[16])
             beta = str(beta_i)
 
@@ -21,11 +61,11 @@ for line in file:
             file_a.write(split_log_line[16].rstrip('\n') + " ")
             file_a.write(split_log_line[18].rstrip('\n') + " ")
             file_a.write(beta.rstrip('\n') + " ")
-
-
+            file_a.write(loc.rstrip('\n') + " ")
+            file_a.write(split_log_line[8].rstrip('\n') + " ")
             switch = False
 
-        if s == 'MOVE' and switch == False:
+        if s == 'MOVE' and switch is False:
             file_a.write(split_log_line[index+1])
             switch = True
 
