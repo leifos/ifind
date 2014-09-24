ce7b1be4417e225befd93118334a6feca5f92539
leifos <leifos@acm.org>
2014-09-24 21:01:56 +0100

    Created an Informed Trec Classifier class that uses the QRELs to make decisions, along with a number of users

---
+++ ifind/apps/simuser/text_classifiers/informed_trec_classifier.py
@@ -0,0 +1,53 @@
+__author__ = 'leif'
+__author__ = 'leif'
+from text_classifiers.base_classifier import BaseTextClassifier
+from ifind.seeker.trec_qrel_handler import TrecQrelHandler
+from random import random
+
+class InformedTrecTextClassifier(BaseTextClassifier):
+    """
+    Takes the  TREC QREL file, and the probabilities rprob and nprob
+    if a document is TREC relevant, then the probability that it is judged relevant is given by rprob
+    if it is NOT TREC releavnt, then the probabiltiy that iit is judged non-relevant is given by nprob
+
+    rprob and nprob are set to 1.0 by default, so that the classifier is deterministic,
+    i.e. it returns the exact TREC relevance judgement
+
+    """
+    def __init__(self, topic, qrel_file, stopword_file=[], background_file=[], rprob=1.0, nprob=1.0):
+        """
+
+        """
+        super(InformedTrecTextClassifier, self).__init__(topic, stopword_file, background_file)
+
+        self._trecqrels = TrecQrelHandler(qrel_file)
+        self._rel_prob = rprob
+        self._nrel_prob = nprob
+
+
+
+    def make_topic_language_model(self):
+        """
+
+        """
+        print "No Topic model required for Trec Classifier"
+
+    def is_relevant(self, document):
+        """
+
+        """
+
+        val = self._trecqrels.get_value(self._topic.id, document.doc_id)
+
+        dp = random()
+
+        if val > 0:
+            if dp > self._rel_prob:
+                return False
+            else:
+                return True
+        else:
+            if dp > self._nrel_prob:
+                return True
+            else:
+                return False
