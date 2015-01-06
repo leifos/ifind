41e71895568dcd6ceeb3690611a97db4c40f9918
David Maxwell <maxwelld90@gmail.com>
2015-01-06 17:46:22 +0000

    Updated predetermined query generator to reflect new query file format

--- ifind/apps/simuser/query_generators/predetermined_query_generator.py
+++ ifind/apps/simuser/query_generators/predetermined_query_generator.py
@@ -4,7 +4,7 @@ from query_generators.base_generator import BaseQueryGenerator
 
 class PredeterminedQueryGenerator(BaseQueryGenerator):
     """
-    Not really a query generator per se..self.
+    Not really a query generator per se...
     but given a list of queries from a configuration file, returns the query list in the specified order.
     
     Requires the following attributes:
@@ -12,9 +12,8 @@ class PredeterminedQueryGenerator(BaseQueryGenerator):
         query_file (string representing path to query file)
         user (string representing the user to focus on)
     
-    The query file should be in the format
-        USERID TOPIC QUERY
-        Separated by spaces - first two spaces are important, all spaces after are considered part of the query.
+    The query file should be in the format (note CSV!)
+        queryid,userid,topic,terms
     """
     def __init__(self, output_controller, stopword_file, query_file, user, background_file=[], topic_model=0):
         """
@@ -29,18 +28,21 @@ class PredeterminedQueryGenerator(BaseQueryGenerator):
         Returns the list of predetermined queries for the specified user.
         """
         queries = []
-        query_file = open(self.__query_filename, 'r')
+        queries_file = open(self.__query_filename, 'r')
         
-        for line in query_file:
+        for line in queries_file:
             line = line.strip()
-            line = line.split()
+            line = line.split(',')
             
-            query_userid = line[0]
-            query_topic = line[1]
-            query_terms = ' '.join(line[2:])
+            line_qid = line[0]
+            line_user = line[1]
+            line_topic = line[2]
+            line_terms = ' '.join(line[3:])
             
-            if query_userid == self.__user and query_topic == topic.id:
-                queries.append((query_terms, 0))
+            if line_user == self.__user and line_topic == topic.id:
+                queries.append((line_terms, int(line_qid)))
         
-        query_file.close()
+        queries_file.close()
+        
+        sorted(queries, key=lambda x: x[1])
         return queries
