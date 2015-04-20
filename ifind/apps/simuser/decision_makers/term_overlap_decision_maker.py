fd31fa6836b3a5bd3378903b8824ca6e4208d7ba
David Maxwell <david@saparua.dcs.gla.ac.uk>
2015-04-20 12:12:42 +0100

    Added term overlap decision maker

---
+++ ifind/apps/simuser/decision_makers/term_overlap_decision_maker.py
@@ -0,0 +1,88 @@
+from loggers import Actions
+from lxml.html.clean import Cleaner
+from decision_makers import kl_divergence
+from decision_makers.base_decision_maker import BaseDecisionMaker
+
+class TermOverlapDecisionMaker(BaseDecisionMaker):
+    """
+    A concrete implementation of a decision maker.
+    Uses term overlap counts from previously seen snippets and the current snippet to determine when to stop.
+    """
+    def __init__(self, search_context, stopword_file, threshold):
+        super(DifferenceDecisionMaker, self).__init__(search_context)
+        
+        self.__stopwords = self.__get_stopwords_list(stopword_file)
+        self.__threshold = threshold
+        
+    def decide(self):
+        """
+        Determines whether the user should proceed to examine the subsequent snippet, or stop and issue a new query.
+        """
+
+        seen_text = ""
+        existing = []
+
+        if self.__query_based:  # If this is query-based, we look at only snippets that were examined in the current query.
+            existing = self._search_context.get_examined_snippets()
+        else:
+            existing = self._search_context.get_all_examined_snippets()
+
+        if self.__nonrel_only:  # Filter to only nonrelevant documents using a list comprehension.
+            existing = [snippet for snippet in existing if snippet.judgment < 1]
+
+        #if zero or one snippets have been examined, then move to the next snippet.
+        if len(existing) <= 1:
+            return Actions.SNIPPET
+
+        current_snippet = existing[-1]
+        remaining_snippets = existing[:-1]
+
+        new_text = "{0} {1}".format(current_snippet.title, self.__clean_markup(current_snippet.content))
+
+        for snippet in remaining_snippets:
+            seen_text = "{0} {1} {2}".format(seen_text, snippet.title, self.__clean_markup(snippet.content))
+        topic = self._search_context.get_topic()
+        seen_text = "{0} {1} {2}".format(seen_text, topic.title, self.__clean_markup(snippet.content))
+        seen_text = "{0} {1} {2}".format(seen_text, topic.content, self.__clean_markup(snippet.content))
+
+
+        kl_score = kl_divergence( new_text,seen_text)
+        print "kl", kl_score
+        #print "SEEN:", seen_text
+        #print "NEW:", new_text
+        #print
+        #raw_input()
+
+        if kl_score < self.__threshold:
+        # if the new text is too similar to the seen text then move to the next query
+            return Actions.QUERY  # Too similar?
+        # else move to the next snippet.
+        return Actions.SNIPPET  #  Different enough, so proceed to examine the next snippet.
+
+    
+    def __clean_markup(self, string_repr):
+        """
+        Given a string representation of a document or snippet, removes all HTML markup and returns it, cleaned.
+        """
+        if string_repr == "":
+            return string_repr
+        
+        cleaner = Cleaner(allow_tags=[''], remove_unknown_tags=False)
+        cleaned_text = cleaner.clean_html(string_repr)
+        
+        return cleaned_text[5:][:-6]  # Removes the extra <div>...</div> that is added
+    
+    def __get_stopwords_list(self, stopwords_filename):
+        """
+        Given the stopwords instance variable, returns a list of stopwords to use.
+        Assumes that each word to be used is on a new line.
+        """
+        stopwords_file = open(stopwords_filename, 'r')
+        stopwords = []
+        
+        for line in stopwords_file:
+            line = line.strip()
+            stopwords.append(line)
+        
+        stopwords_file.close()
+        return stopwords
