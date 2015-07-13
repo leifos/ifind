566699675c2b4c194414ff4e47fbcd42258b16dc
Mickey Pashov <pashov.m@gmail.com>
2015-07-13 20:24:09 +0100

    Can now work out word probability within a snippet

--- ifind/apps/snippets/utils.py
+++ ifind/apps/snippets/utils.py
@@ -133,23 +133,26 @@ def extract_entities(snippet_list):
         pass
 
 
-def reduce_snippet():
-
-    # build a list of term frequencies (look at nltk)
-    # if you dont have a background vocabulary, take the top 50 or 100 bing results,
-    # i.e. all the snippets. Then count the number of times each term occurs. Store this in a dict()
-    btc = dict()
-    snippet_list = []
-    for word in snippet_list():
-        btc[word] = frequency_count(word, snippet_list)
+# def reduce_snippet():
+#
+#     # build a list of term frequencies (look at nltk)
+#     # if you dont have a background vocabulary, take the top 50 or 100 bing results,
+#     # i.e. all the snippets. Then count the number of times each term occurs. Store this in a dict()
+#     btc = dict()
+#     snippet_list = []
+#     for word in snippet_list():
+#         btc[word] = frequency_count(word, snippet_list)
 
 
 def frequency_count(filename):
     """
-    Extracts a list of term frequencies from a text file (aquaint.txt)
+    Extracts a list of words and their frequencies from a text file (aquaint.txt)
     :param filename:
     """
     btc = dict()
+    # with open('filename') as f:
+    # lines = f.readlines()
+
     lines = [line.rstrip('\n') for line in open(filename)]
 
     for line in lines:
@@ -160,4 +163,57 @@ def frequency_count(filename):
 
         btc[word] = int(frequency)
 
-    print btc
+    return btc
+
+def frequency_count2(filename):
+    """
+    Generates word frequencies from a textfile (snippets.txt)
+    :param filename:
+    :return:
+    """
+
+    file_in = open(filename, "r+")
+
+    wordcount = {}
+    for word in file_in.read().split():
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
+
+    for k, v in wordcount.items():
+        print k, v
+
+    file_in.close()
+
+
+def probs(snippet, term_frequency_file):
+    """
+        Evaluates the probability of the words in a snippet
+        based on term frequencies provided
+    :param snippet:
+    :return:
+    """
+
+    word_counts = frequency_count(term_frequency_file)
+    total_word_count = 1.0
+
+    for word in word_tokenize(snippet):
+        if not word in word_counts:
+            word_counts[word.lower()] = 1
+            for i in word_counts:
+                word_counts[i] += 1
+        total_word_count += word_counts[word]
+
+    word_probs = {}
+
+    for word in word_tokenize(snippet):
+        probability = word_counts[word] / total_word_count
+
+        word_probs[word] = probability
+
+    for k, v in word_counts.items():
+        print k, v
+
+    for k, v in word_probs.items():
+        print k, v
