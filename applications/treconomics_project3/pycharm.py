939a17e54bde98680c545179f926921248718d3c
Mickey Pashov <pashov.m@gmail.com>
2015-07-31 20:08:47 +0100

    PyCharm with iFind hack

---
+++ applications/treconomics_project3/pycharm.py
@@ -0,0 +1,5 @@
+import sys
+# Importing ifind
+sys.path.append("/Users/mickeypash/ifind/")
+
+# export PYTHONPATH=/Users/mickeypash/ifind/:/Users/mickeypash/ifind/applications/slowsearch_project:$PYTHONPATH
