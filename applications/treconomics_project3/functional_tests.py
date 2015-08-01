314ee7448cc4c2c7a226d5bc6d2a6b708dbd79b9
Mickey Pashov <pashov.m@gmail.com>
2015-08-01 15:38:25 +0100

    Adding a user story to make testing more streamlined

--- applications/treconomics_project3/functional_tests.py
+++ applications/treconomics_project3/functional_tests.py
@@ -18,5 +18,22 @@ class HomePageTest(unittest.TestCase):
         print 'Test passes'
 
 
+    # Onika starts the experiment by entering her u: test12 and p: 12
+    # She presses Login, a new page is opened!
+    # She reads the instructions provided and presses Next
+    # She reads the Practice Task instructions and presses Next
+
+    # She presses Show Task
+    # She closes the new window after reading the task
+
+    # She enters relevant query terms and presses Search
+    # She hovers over the first few results and selects one
+    # She decides to Mark it as Relevant
+    # She decides to View Saved
+    # She decides to go Back to Results
+    # She decides to End Task
+    # She reads the short debrief and presses Next
+
+
 if __name__ == '__main__':
     unittest.main()
