f5c86803aea4e175ca206ab0e405f974b3ff2ca2
Mickey Pashov <pashov.m@gmail.com>
2015-08-04 01:11:53 +0100

    Whoosh BM25f word scoring works

--- applications/treconomics_project3/snippets/summarization.py
+++ applications/treconomics_project3/snippets/summarization.py
@@ -2,7 +2,9 @@ __author__ = 'mickeypash'
 from pyteaser import Summarize
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
+from whoosh import scoring
 from blessings import Terminal
+import re
 import itertools
 import os
 
@@ -13,17 +15,31 @@ ix = open_dir(my_whoosh_doc_index_dir)
 
 t = Terminal()
 
-parser = QueryParser("content", ix.schema)
-myquery = parser.parse('airport')
+with ix.searcher(weighting=scoring.BM25F()) as mysearcher:
 
-with ix.searcher() as mysearcher:
-    results = mysearcher.search(myquery)
-    for i, hit in enumerate(itertools.islice(results, 0, 1)):
-        title = hit["title"]
-        document = hit["content"]
-        summaries = Summarize(title, document)
+    qp = QueryParser("content", ix.schema)
+    query_list = ['airport', 'wildlife', 'privacy', 'news']
 
-        print title
-        for num, summary in enumerate(summaries):
-            with open("summary{0}.txt".format(num), "w+") as fn:
-                fn.write(summary)
+    for query in itertools.islice(query_list, 0, 10):
+
+        q = qp.parse(unicode('id:1'))
+        bm25f = scoring.BM25F()
+
+        matcher = q.matcher(mysearcher)
+        bm25f_scorer = bm25f.scorer(mysearcher, 'alltext', query)
+
+        score = bm25f_scorer.score(matcher)
+        print query, score
+
+        # myquery = qp.parse(query)
+        # results = mysearcher.search(myquery)
+        #
+        # results.fragmenter.charlimit = None
+        #
+        # for i, hit in enumerate(itertools.islice(results, 0, 20)):
+        #     title = hit["title"]
+        #     document = hit["content"]
+        #     summaries = Summarize(title, document)
+        #
+        #     print '%d %s' % (i+1, title.title())
+        #     print t.red(re.sub('<[^<]+?>', '', hit.highlights("content")))
