9276168ae5fa8ece5ae1302c27f137dd662b9348
David Maxwell <maxwelld90@gmail.com>
2015-08-21 20:07:07 +0100

    Updated text classifiers

--- ifind/apps/simuser/text_classifiers/informed_trec_classifier.py
+++ ifind/apps/simuser/text_classifiers/informed_trec_classifier.py
@@ -1,55 +1,24 @@
 __author__ = 'leif'
 
-from text_classifiers.base_classifier import BaseTextClassifier
+from text_classifiers.base_informed_trec_classifier import BaseInformedTrecTextClassifier
 from ifind.seeker.trec_qrel_handler import TrecQrelHandler
 from random import random
 
-class InformedTrecTextClassifier(BaseTextClassifier):
+class InformedTrecTextClassifier(BaseInformedTrecTextClassifier):
     """
-    Takes the TREC QREL file, and the probabilities rprob and nprob
-    if a document is TREC relevant, then the probability that it is judged relevant is given by rprob
-    if it is NOT TREC releavnt, then the probabiltiy that iit is judged non-relevant is given by nprob
-
-    rprob and nprob are set to 1.0 by default, so that the classifier is deterministic,
-    i.e. it returns the exact TREC relevance judgement
-
+    A concrete implementation of BaseInformedTrecTextClassifier.
+    Loads QRELS from disk.
     """
     def __init__(self, topic, qrel_file, stopword_file=[], background_file=[], rprob=1.0, nprob=1.0):
         """
 
         """
-        super(InformedTrecTextClassifier, self).__init__(topic, stopword_file, background_file)
-
-        self._trecqrels = TrecQrelHandler(qrel_file)
-        self._rel_prob = rprob
-        self._nrel_prob = nprob
-
-
-
-    def make_topic_language_model(self):
-        """
-
-        """
-        print "No Topic model required for Trec Classifier"
-
-    def is_relevant(self, document):
-        """
-
-        """
-        val = self._trecqrels.get_value_if_exists(self._topic.id, document.doc_id)  # Does the document exist?
+        super(InformedTrecTextClassifier, self).__init__(topic, qrel_file, stopword_file, background_file, rprob=rprob, nprob=nprob)
         
-        if not val:  # If not, we fall back to the generic topic.
-            val = self._trecqrels.get_value('0', document.doc_id)
-        
-        dp = random()
-        
-        if val > 0:
-            if dp > self._rel_prob:
-                return False
-            else:
-                return True
-        else:
-            if dp > self._nrel_prob:
-                return True
-            else:
-                return False
+    
+    def _initialise_handler(self, qrel_file):
+        """
+        This is spun off from the constructor to make way for the Redis classifier.
+        Contrete implementation of the base class!
+        """
+        self._trecqrels = TrecQrelHandler(qrel_file)  # Easy!
