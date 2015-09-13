b1c9178ec7588508cdbf31b8f61e60eee9b7b232
leifos <leifos@acm.org>
2015-09-13 16:22:19 +0100

    All applications moved to ifindapps

--- applications/treconomics_project/data/scripts/make_aquaint_file_set.sh
+++
@@ -1,2 +0,0 @@
-find /Users/leif/Code/AQUAINT/data/xml -type f -name "*.xml" > aquaint_xml_files
-
--- applications/treconomics_project3/pycharm.py
+++
@@ -1,5 +0,0 @@
-import sys
-# Importing ifind
-sys.path.append("/Users/mickeypash/ifind/")
-
-# export PYTHONPATH=/Users/mickeypash/ifind/:/Users/mickeypash/ifind/applications/slowsearch_project:$PYTHONPATH
