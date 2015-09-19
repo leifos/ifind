f58e26c0128892eff74a7694859162ffbe8a6967
leifos <leifos@acm.org>
2015-09-19 19:25:35 +0100

    updated result handler for trec results

--- ifind/seeker/trec_result_handler.py
+++ ifind/seeker/trec_result_handler.py
@@ -7,19 +7,19 @@ from ifind.seeker.topic_document_file_handler import TopicDocumentFileHandler
 
 
 def process_trec_line(line):
-# handles the specific format of the line - assumes 6 columns TREC Result format
-	# topic QO document rank score EXP
-	parts = line.partition(' ')
-	topic = parts[0]
-	parts = parts[2].partition(' ')
-	parts = parts[2].partition(' ')
-	doc = parts[0]
-	parts = parts[2].partition(' ')
-	rank = parts[0]
-	parts = parts[2].partition(' ')
-	score = parts[0]
+    # handles the specific format of the line - assumes 6 columns TREC Result format
+    # topic QO document rank score EXP
+    parts = line.partition(' ')
+    topic = parts[0]
+    parts = parts[2].partition(' ')
+    parts = parts[2].partition(' ')
+    docid = parts[0]
+    parts = parts[2].partition(' ')
+    rank = parts[0]
+    parts = parts[2].partition(' ')
+    score = parts[0]
 
-	return (topic, doc, rank, score)
+    return (topic, docid, rank, score)
 
 
 class TrecResultHandler(TopicDocumentFileHandler):
@@ -28,19 +28,26 @@ class TrecResultHandler(TopicDocumentFileHandler):
         super(TrecResultHandler, self).__init__(filename)
 
     def _put_in_line(self, line):
-        topic, doc, rank, score = process_trec_line(line)
-        if topic and doc:
-            self.data[topic][doc] =  (int(rank), float(score))
+        topic, docid, rank, score = process_trec_line(line)
+        if topic and docid:
+            self.data[topic][docid] = [docid, float(score)]
 
-    def _get_out_line(self, topic, doc):
+    def _get_out_line(self, topic, doc, rank, score):
         # outputs in TREC Result format
-        return "%s Q0 %s %d %f EXP\n" % (topic, doc.strip(), self.get_value(topic,doc), self.get_score(topic,doc))
+        return "{0} Q0 {1} {2} {3} EXP\n".format(topic, doc.strip(), rank, score)
 
     def get_score(self, topic, doc):
         if self.data[topic][doc]:
             return self.data[topic][doc][1]
         else:
             return 0.0
+
+    def update_score(self, topic, doc, score):
+        if self.data[topic][doc]:
+            self.data[topic][doc][1] = score
+            return True
+        return False
+
 
     def get_value(self, topic, doc):
         if self.data[topic][doc]:
@@ -59,8 +66,8 @@ class TrecResultHandler(TopicDocumentFileHandler):
         udl = self.get_doc_list(topic)
         dl = []
         for d in udl:
-            dl.append((d, self.get_rank(topic,d), self.get_score(topic,d)))
-        odl = sorted(dl, key=lambda doc: doc[1])
+            dl.append((d, self.get_score(topic,d)))
+        odl = sorted(dl, key=lambda doc: doc[1],reverse=True)
 
         return odl
 
@@ -74,11 +81,13 @@ class TrecResultHandler(TopicDocumentFileHandler):
 
         for t in self.get_topic_list():
             odl = self.get_ranking(t)
+            rank = 1
             for d in odl:
-                out_line = self._get_out_line(t,d[0])
+                out_line = self._get_out_line(t,d[0], rank, d[1])
+                rank += 1
                 outfile.write (out_line)
 
         outfile.close()
 
     def clear(self):
-	    self.data = AutoVivification()
+        self.data = AutoVivification()
